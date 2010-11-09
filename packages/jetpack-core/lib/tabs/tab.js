0737295effb7c5f0afe7b66a8406b8c8e24df96e
Irakli Gozalishvili <rfobic@gmail.com>
2010-11-09 18:25:40 +0100

    Implementing all of the functionality for tabs module and makeing in i10s compatible (still need to update docs & tests)

---
+++ packages/jetpack-core/lib/tabs/tab.js
@@ -0,0 +1,193 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Jetpack.
+ *
+ * The Initial Developer of the Original Code is Mozilla.
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Irakli Gozalishvili <gozala@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+"use strict";
+
+const { Trait } = require("traits");
+const { EventEmitter } = require("events");
+const dataUtils = require("utils/data");
+const thumbnailUtils = require("utils/thumbnail");
+
+const PREFIX = "on";
+const DOM_READY = "DOMContentLoaded";
+const ON_READY = "ready";
+const ON_TAB_OPEN = "TabOpen";
+const ON_TAB_CLOSE = "TabClose";
+const ON_TAB_SELECT = "TabSelect";
+
+const EVENTS = [
+  [ PREFIX + ON_TAB_OPEN, ON_TAB_OPEN ],
+  [ PREFIX + ON_TAB_CLOSE, ON_TAB_CLOSE ],
+  [ PREFIX + ON_TAB_SELECT, ON_TAB_SELECT ]
+];
+
+// Array the inner instances of all the wrapped tabs.
+const TABS = [];
+
+/**
+ * Trait used to wrap tap elements.
+ */
+const TabTrait = Trait.compose(EventEmitter, {
+  on: Trait.required,
+  _emit: Trait.required,
+
+  /**
+   * Tab DOM element that is being wrapped.
+   */
+  _tab: null,
+  window: null,
+
+  constructor: function Tab(options) {
+    this._onReady = this._onReady.bind(this)
+    this._tab = options.tab;
+    let window = this.window = options.window;
+    // setting event listener if required
+    if ('onReady' in options)
+      this.on(ON_READY, options.onReady)
+
+    for each (let [ name, type ] in EVENTS) {
+      window.on(type, this._onEvent.bind(this, type))
+      if (name in options)
+        this.on(type, options[name]);
+    }
+
+    
+    let window = this._contentWindow;
+    this._contentWindow.addEventListener(DOM_READY, this._onReady, true);
+    // listen for events, filtered on this tab
+    return this;
+  },
+  _onReady: function _onReady(event) {
+    this._contentWindow.removeEventListener(DOM_READY, this._onReady, true);
+    this._emit(ON_READY, this._public);
+  },
+  _onEvent: function _onEvent(type, tab) {
+    //tabs._emit(type, tab);
+    if (tab == this._public)
+      this._emit(type, tab);
+  },
+  /**
+   * Browser DOM element where page of this tab is currently loaded.
+   */
+  get _browser() this._window.gBrowser.getBrowserForTab(this._tab),
+  /**
+   * Window DOM element containing this tab.
+   */
+  get _window() this._tab.ownerDocument.defaultView,
+  /**
+   * Document object of the page that is currently loaded in this tab.
+   */
+  get _contentDocument() this._browser.contentDocument,
+  /**
+   * Window object of the page that is currently loaded in this tab.
+   */
+  get _contentWindow() this._browser.contentWindow,
+
+  // Has been exposed by previous implementation, but i10s incompatible
+  // and probably will be removed.
+  get contentDocument() this._contentDocument,
+  get contentWindow() this._contentWindow,
+  
+  /**
+   * The title of the page currently loaded in the tab.
+   * Changing the property will change a title.
+   * @type {String}
+   */
+  get title() this._contentDocument.title,
+  set title(value) this._contentDocument.title = String(value),
+  /**
+   * Location of the page currently loaded in this tab.
+   * Changing value of this property will load different page in this tab.
+   * @type {String}
+   */
+  get location() String(this._contentDocument.location),
+  set location(value) this._contentWindow.location = String(value),
+  /**
+   * URI of the favicon for the page currently loaded in this tab.
+   * @type {String}
+   */
+  get faveicon() dataUtils.favicon(String(this.location)),
+  /**
+   * The CSS style for the tab
+   */
+  get style() null, // TODO
+  /**
+   * The index of the tab relative to other tabs in the application window.
+   * Changing this property will change order of the actual position of the tab.
+   * @type {Number}
+   */
+  get index()
+    this._window.gBrowser.getBrowserIndexForDocument(this._contentDocument),
+  set index(value) this._window.gBrowser.moveTabTo(this._tab, value),
+  /**
+   * Thumbnail data URI of the page currently loaded in this tab.
+   * @type {String}
+   */
+  get thumbnail() thumbnailUtils.thumbnailURI(this._contentWindow),
+  /**
+   * Weather or not tab is pinned (Is an app-tab).
+   * Changing value will pin / unpin tab.
+   * @type {Boolean}
+   */
+  get pinned() this._tab.pinned,
+  set pinned(value) {
+    if (!!value) this._window.gBrowser.pinTab(this._tab);
+    else this._window.gBrowser.unpinTab(this._tab);
+  },
+  /**
+   * Make this tab active.
+   */
+  focus: function focus() {
+    this._window.gBrowser.selectedTab = this._tab;
+    return this._public;
+  },
+  /**
+   * Close the tab
+   */
+  close: function close() this._window.gBrowser.removeTab(this._tab)
+});
+
+function Tab(options) {
+  let chromeTab = options.tab;
+  for each (let tab in TABS) {
+    if (chromeTab == tab._tab)
+      return tab._public
+  }
+  let tab = TabTrait(options);
+  TABS.push(tab);
+  return tab._public;
+}
+Tab.prototype = TabTrait.prototype
+exports.Tab = Tab
--- packages/jetpack-core/lib/windows/tabs.js
+++ packages/jetpack-core/lib/windows/tabs.js
@@ -34,50 +34,54 @@
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
-
 "use strict";
 
-const { Cc, Ci, Cu } = require("chrome");
 const { Trait } = require("traits");
 const { List } = require("list");
+const { Tab } = require("tabs/tab");
+const { EventEmitter } = require("events");
 
-const ON_READY = "DOMContentLoaded";
+const DOM_READY = "DOMContentLoaded";
 const DOM_TAB_OPEN = "TabOpen";
 const DOM_TAB_CLOSE = "TabClose";
+const DOM_TAB_SELECT = "TabSelect";
 const ON_TAB_OPEN = "TabOpen";
 const ON_TAB_CLOSE = "TabClose";
+const ON_TAB_SELECT = "TabSelect";
 const TAB_BROWSER = "tabbrowser";
-const DEF_FAVICON = "chrome://mozapps/skin/places/defaultFavicon.png"
-const DATA_PNG_B64 = "data:image/png;base64,";
 
-let NetUtil = {};
-Cu.import("resource://gre/modules/NetUtil.jsm", NetUtil);
-NetUtil = NetUtil.NetUtil;
-const FaveiconService = Cc["@mozilla.org/browser/favicon-service;1"].
-                          getService(Ci.nsIFaviconService);
 
-const TabList = List.resolve({ constructor: "_init" }).compose(
-  Trait.compose({
-    constructor: function TabList(window) {
-      window.on(ON_TAB_OPEN, this._add.bind(this));
-      window.on(ON_TAB_CLOSE, this._remove.bind(this));
-      this._init();
-    }
-  }).resolve({ toString: null })
-);
+const TAB_CONTAINER_EVENTS = [
+  [DOM_TAB_OPEN, ON_TAB_OPEN],
+  [DOM_TAB_CLOSE, ON_TAB_CLOSE],
+  [DOM_TAB_SELECT, ON_TAB_SELECT]
+]
 
 const WindowTabTracker = Trait.compose({
+  /**
+   * Chrome window whose tabs are tracked.
+   */
   _window: Trait.required,
+  /**
+   * Callback that is called once first tab is fully loaded.
+   */
   _onReady: Trait.required,
+  /**
+   * Function used to emit 'TabOpen' & 'TabClose' events.
+   */
   _emit: Trait.required,
+  on: Trait.required,
+  /**
+   * Live array of the windows tabContainers.
+   */
   get _tabContainers()
     Array.slice(this._window.document.getElementsByTagName(TAB_BROWSER))
       .map(function(tabBrowser) tabBrowser.tabContainer),
+  /**
+   * Initializes tab tracker.
+   */
   _initWindowTabTracker: function _initWindowTabTracker() {
     this.tabs;
-    // Binding event listeners to the instance.
-    this._onTabOpen = this._onTabOpen.bind(this);
-    this._onTabClose = this._onTabClose.bind(this);
     let firstTab = null;
     // Some XULRunner apps may have more then one tab browser.
     for each (let tabContainer in this._tabContainers) {
@@ -85,114 +89,63 @@ const WindowTabTracker = Trait.compose({
       let tabs = Array.slice(tabContainer.children);
       for each (let tab in tabs) {
         firstTab = firstTab || tab;
-        this._onTabOpen({ target: tab })
+        this._onTabEvent(ON_TAB_OPEN, { target: tab })
       }
-      // Setting event listeners to track opennig / closing tabs.
-      tabContainer.addEventListener(DOM_TAB_OPEN, this._onTabOpen, true);
-      tabContainer.addEventListener(DOM_TAB_CLOSE, this._onTabClose, true);
+      // Setting event listeners to track tab events
+      for each (let [domType, eventType] in TAB_CONTAINER_EVENTS) {
+        tabContainer.addEventListener(domType,
+                                      this._onTabEvent.bind(this, eventType),
+                                      true);
+      }
     }
     let gBrowser = this._window.gBrowser;
     let tab = gBrowser.getBrowserForTab(firstTab);
     let listener = this.__onReady = this.__onReady.bind(this, tab)
-    tab.addEventListener(ON_READY, listener, false);
+    tab.addEventListener(DOM_READY, listener, false);
   },
   __onReady: function __onReady(tab, e) {
-    tab.removeEventListener(ON_READY, this.__onReady, false);
+    tab.removeEventListener(DOM_READY, this.__onReady, false);
     this._onReady(e);
   },
-  _onTabOpen: function _onTabOpen(event) {
-    this._emit(ON_TAB_OPEN, Tab({ tab: event.target }));
-  },
-  _onTabClose: function _onTabClose(event) {
-    this._emit(ON_TAB_CLOSE, Tab({ tab: event.target }));
+  _onTabEvent: function _onTabEvent(type, event) {
+    var tab = Tab({ tab: event.target, window: this._public });
+    this._emit(type, tab);
+    this._tabs._emit(type, tab);
+    tabs._emit(type, tab);
   }
 });
 exports.WindowTabTracker = WindowTabTracker;
+
+const TabList = List.resolve({ constructor: "_init" }).compose(
+  EventEmitter.resolve({ toString: null }),
+  Trait.compose({
+    on: Trait.required,
+    _emit: Trait.required,
+    constructor: function TabList() {
+      this.on(ON_TAB_OPEN, this._add.bind(this));
+      this.on(ON_TAB_CLOSE, this._remove.bind(this));
+      this._init();
+      return this;
+    }
+  }).resolve({ toString: null })
+);
+
+// Combined list of tabs for all the windows.
+var tabs = TabList();
+exports.tabs = tabs._public;
 
 const WindowTabs = Trait.compose(
   WindowTabTracker,
   Trait.compose({
     _window: Trait.required,
     get tabs() {
-      return this._tabs || (this._tabs = TabList(this._public))
+      return (this._tabs || (this._tabs = TabList()))._public
     },
-    _tabs: null
+    _tabs: null,
+
+    get activeTab()
+      Tab({ tab: this._window.gBrowser.selectedTab, window: this._public }),
+    set activeTab(value) value.focus()
   })
-)
+);
 exports.WindowTabs = WindowTabs;
-
-
-const TabTrait = Trait.compose({
-  _tab: null,
-
-  constructor: function Tab(options) {
-    this._tab = options.tab;
-    // listen for events, filtered on this tab
-    // eventsTabDelegate.addTabDelegate(this);
-    return this;
-  },
-
-  get _browser() this._window.gBrowser.getBrowserForTab(this._tab),
-  get _window() this._tab.ownerDocument.defaultView,
-  get _contentDocument() this._browser.contentDocument,
-  get _contentWindow() this._browser.contentWindow,
-
-  // Non i10s compatible and will go away soon.
-  get contentDocument() this._contentDocument,
-  get contentWindow() this._contentWindow,
-
-  get title() this._contentDocument.title,
-  get location() this._contentDocument.location,
-  get faveicon() {
-    let pageURI = NetUtil.newURI(this.location);
-    let faviconURL;
-    try {
-      let faviconURI = FaveiconService.getFaviconForPage(pageURI);
-      faviconURL = FaveiconService.getFaviconDataAsDataURL(faviconURI);
-    } catch(ex) {
-      let data = getChromeURLContents(DEF_FAVICON);
-      let encoded = this._contentWindow.btoa(data);
-      faviconURL = DATA_PNG_B64 + encoded;
-    }
-    return faviconURL;
-  },
-  get style() null, // TODO
-  get index()
-    this._window.gBrowser.getBrowserIndexForDocument(this._contentDocument),
-  get thumbnail()
-    getThumbnailCanvasForTab(this._tab, this._contentWindow),
-  get isPinned() this._tab.pinned,
-
-  pin: function pin() {
-    this._window.gBrowser.pinTab(this._tab);
-    return this._public;
-  },
-  unpin: function unpin() {
-    this._window.gBrowser.unpinTab(this._tab)
-    return this._public;
-  },
-  focus: function focus() {
-    let window = this._window;
-    window.focus();
-    window.gBrowser.selectedTab = this._tab;
-    return this._public;
-  },
-  move: function move(index) {
-    this._window.gBrowser.moveTabTo(this._tab, index);
-    return this._public;
-  }
-});
-
-const TABS = []
-function Tab(options) {
-  let chromeTab = options.tab;
-  for each (let tab in TABS) {
-    if (chromeTab == tab._tab)
-      return tab._public
-  }
-  let tab = TabTrait(options);
-  TABS.push(tab);
-  return tab._public;
-}
-Tab.prototype = TabTrait.prototype
-exports.Tab = Tab
