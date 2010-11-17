mozilla/addon-sdk

commit d63ec94e924f80421ab7ae7b7dc23a4f441e4cd1

Renaming few util functions to follow nameing conventios that were suggested an adding missing semicolons.

packages/jetpack-core/lib/utils/data.js
@@ -43,10 +43,9 @@ const IOService = Cc["@mozilla.org/network/io-service;1"].
 const AppShellService = Cc["@mozilla.org/appshell/appShellService;1"].
   getService(Ci.nsIAppShellService);
 
-let NetUtil = {};
-Cu.import("resource://gre/modules/NetUtil.jsm", NetUtil);
+Cu.import("resource://gre/modules/NetUtil.jsm", this);
 NetUtil = NetUtil.NetUtil;
-const FaveiconService = Cc["@mozilla.org/browser/favicon-service;1"].
+const FaviconService = Cc["@mozilla.org/browser/favicon-service;1"].
                           getService(Ci.nsIFaviconService);
 
 const PNG_B64 = "data:image/png;base64,";
@@ -60,15 +59,18 @@ let   DEF_FAVICON = null;
  * @param {String} uri
  * @returns {String}
  */
-exports.favicon = function favicon(uri) {
+exports.getFaviconURIForLocation = function getFaviconURIForLocation(uri) {
   let pageURI = NetUtil.newURI(uri);
   try {
-    return FaveiconService.getFaviconDataAsDataURL(
-                  FaveiconService.getFaviconForPage(pageURI));
-  } catch(e) {
-    if (!DEF_FAVICON)
-      DEF_FAVICON = PNG_B64 + base64Encode(chromeURIContent(DEF_FAVICON_URI))
-    return DEF_FAVICON
+    return FaviconService.getFaviconDataAsDataURL(
+                  FaviconService.getFaviconForPage(pageURI));
+  }
+  catch(e) {
+    if (!DEF_FAVICON) {
+      DEF_FAVICON = PNG_B64 +
+                    base64Encode(getChromeURIContent(DEF_FAVICON_URI));
+    }
+    return DEF_FAVICON;
   }
 }
 
@@ -77,7 +79,7 @@ exports.favicon = function favicon(uri) {
  * @param {String} chromeURI
  * @returns {String}
  */
-function chromeURIContent(chromeURI) {
+function getChromeURIContent(chromeURI) {
   let channel = IOService.newChannel(chromeURI, null, null);
   let input = channel.open();
   let stream = Cc["@mozilla.org/binaryinputstream;1"].
@@ -88,19 +90,16 @@ function chromeURIContent(chromeURI) {
   input.close();
   return content;
 }
-exports.chromeURIContent = chromeURIContent
+exports.getChromeURIContent = getChromeURIContent;
 
 /**
  * Creates a base-64 encoded ASCII string from a string of binary data.
  */
-function base64Encode(data) {
-  return AppShellService.hiddenDOMWindow.btoa(String(data))
-}
-exports.base64Encode = base64Encode
+function base64Encode(data) AppShellService.hiddenDOMWindow.btoa(String(data));
+exports.base64Encode = base64Encode;
 
 /**
  * Decodes a string of data which has been encoded using base-64 encoding.
  */
-exports.base64Decode = function base64Decode(data) {
-  return AppShellService.hiddenDOMWindow.atob(String(data))
-}
+function base64Decode(data) AppShellService.hiddenDOMWindow.atob(String(data));
+exports.base64Decode = base64Decode;

packages/jetpack-core/lib/tabs/tab.js
@@ -37,11 +37,11 @@
 
 const { Trait } = require("traits");
 const { EventEmitter } = require("events");
-const dataUtils = require("utils/data");
-const thumbnailUtils = require("utils/thumbnail");
 const { validateOptions } = require("api-utils");
 const { Enqueued } = require("utils/function");
 const { EVENTS } = require("tabs/events");
+const { getThumbnailURIForWindow } = require("utils/thumbnail");
+const { getFaviconURIForLocation } = require("utils/data");
 
 
 
@@ -76,7 +76,7 @@ const TabTrait = Trait.compose(EventEmitter, {
         window.tabs.on(type.name, this._onEvent.bind(this, type.name));
     }
 
-    this.on(EVENTS.close.name, this.destroy.bind(this))
+    this.on(EVENTS.close.name, this.destroy.bind(this));
     this._browser.addEventListener(EVENTS.ready.dom, this._onReady, true);
 
     this.pinned = options.pinned;
@@ -89,7 +89,7 @@ const TabTrait = Trait.compose(EventEmitter, {
   },
   _onError: function _onError(error) {
     if (1 <= this._listeners('error').length)
-      console.exception(error)
+      console.exception(error);
   },
   destroy: function destroy() {
     for each (let type in EVENTS)
@@ -156,7 +156,7 @@ const TabTrait = Trait.compose(EventEmitter, {
    * URI of the favicon for the page currently loaded in this tab.
    * @type {String}
    */
-  get favicon() dataUtils.favicon(String(this.location)),
+  get favicon() getFaviconURIForLocation(String(this.location)),
   /**
    * The CSS style for the tab
    */
@@ -173,7 +173,7 @@ const TabTrait = Trait.compose(EventEmitter, {
    * Thumbnail data URI of the page currently loaded in this tab.
    * @type {String}
    */
-  get thumbnail() thumbnailUtils.thumbnailURI(this._contentWindow),
+  get thumbnail() getThumbnailURIForWindow(this._contentWindow),
   /**
    * Whether or not tab is pinned (Is an app-tab).
    * Changing value will pin / unpin tab.
@@ -204,14 +204,14 @@ function Tab(options) {
   let chromeTab = options.tab;
   for each (let tab in TABS) {
     if (chromeTab == tab._tab)
-      return tab._public
+      return tab._public;
   }
   let tab = TabTrait(options);
   TABS.push(tab);
   return tab._public;
 }
-Tab.prototype = TabTrait.prototype
-exports.Tab = Tab
+Tab.prototype = TabTrait.prototype;
+exports.Tab = Tab;
 
 function Options(options) {
   if ("string" === typeof options)

packages/jetpack-core/lib/utils/thumbnail.js
@@ -49,7 +49,7 @@ const COLOR = "rgb(255,255,255)";
  * @param {Window} window
  * @returns {Element}
  */
-function thumbnailCanvas(window) {
+function getThumbnailCanvasForWindow(window) {
   let aspectRatio = 0.5625; // 16:9
   let thumbnail = AppShellService.hiddenDOMWindow.document
                     .createElementNS(NS, "canvas");
@@ -64,13 +64,13 @@ function thumbnailCanvas(window) {
                 snippetWidth * aspectRatio, COLOR);
   return thumbnail;
 }
-exports.thumbnailCanvas = thumbnailCanvas
+exports.getThumbnailCanvasForWindow = getThumbnailCanvasForWindow;
 
 /**
  * Creates Base64 encoded data URI of the thumbnail for the passed window.
  * @param {Window} window
  * @returns {String}
  */
-exports.thumbnailURI = function thumbnailURI(window) {
-  return thumbnailCanvas(window).toDataURL()
-}
+exports.getThumbnailURIForWindow = function getThumbnailURIForWindow(window) {
+  return getThumbnailCanvasForWindow(window).toDataURL()
+};

packages/jetpack-core/lib/windows/tabs.js
@@ -80,9 +80,10 @@ const WindowTabTracker = Trait.compose({
       let tabs = Array.slice(tabContainer.children);
       // Emulating 'open' events for all open tabs.
       for each (let tab in tabs)
-        this._onTabEvent(EVENTS.open, { target: tab })
-      this._onTabEvent(EVENTS.activate, { target: this._window.gBrowser.selectedTab })
-      // Setting event listeners to track tab events
+        this._onTabEvent(EVENTS.open, { target: tab });
+      this._onTabEvent(EVENTS.activate,
+                       { target: this._window.gBrowser.selectedTab });
+      // Setting event listeners to track tab events.
       for each (let type in EVENTS) {
         if (!type.dom) continue;
         tabContainer.addEventListener(type.dom,
@@ -159,14 +160,14 @@ const TabList = List.resolve({ constructor: "_init" }).compose(
     },
     _onError: function _onError(error) {
       if (1 <= this._listeners('error').length)
-        console.exception(error)
+        console.exception(error);
     },
     get active() this._active,
     set active(active) active.focus(),
     _active: null,
 
     open: function open(options) {
-      options = Options(options)
+      options = Options(options);
       this._window._tabOptions.push(options);
       this._window._window.gBrowser.addTab(options.url);
     }

commit 4b7bc123ac5aa3a73cc949915515967503002c3c

Fixing regression introduced by: d63ec94e924f80421ab7ae7b7dc23a4f441e4cd1

packages/jetpack-core/lib/utils/data.js
@@ -44,7 +44,6 @@ const AppShellService = Cc["@mozilla.org/appshell/appShellService;1"].
   getService(Ci.nsIAppShellService);
 
 Cu.import("resource://gre/modules/NetUtil.jsm", this);
-NetUtil = NetUtil.NetUtil;
 const FaviconService = Cc["@mozilla.org/browser/favicon-service;1"].
                           getService(Ci.nsIFaviconService);
 