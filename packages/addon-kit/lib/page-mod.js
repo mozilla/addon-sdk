85dca12c4b5d58d84ee48b7abfd47de36b2ee4a0
Irakli Gozalishvili <rfobic@gmail.com>
2010-12-01 14:01:51 +0100

    Workaround for a problem of 'document-element-inserted' being triggered with `defaultView` property `null`.

--- packages/addon-kit/lib/page-mod.js
+++ packages/addon-kit/lib/page-mod.js
@@ -180,6 +180,11 @@ const PageModManager = Registry.resolve({
   },
   _onContentWindow: function _onContentWindow(domObj) {
     let window = HAS_DOCUMENT_ELEMENT_INSERTED ? domObj.defaultView : domObj;
+
+    // Workarounds a problem of 'document-element-inserted' being triggered
+    // with `defaultView` property `null`.
+    if (!window) return;
+
     for (let rule in RULES)
       if (RULES[rule].test(window.document.URL))
         this._emit(rule, window);
