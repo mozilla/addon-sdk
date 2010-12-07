aa79df57140df7dec20f8f93ff8c13253afde24d
Myk Melez <myk@mozilla.org>
2010-12-07 07:00:17 -0800

    revert private-browsing changes to event listener parameters (cherry picked from commit 976f5a2ee44e3a0dd52591fdaebceb16a69d27f5)

--- packages/addon-kit/tests/test-private-browsing.js
+++ packages/addon-kit/tests/test-private-browsing.js
@@ -73,11 +73,8 @@ if (pbService) {
 
   exports.testStart = function(test) {
     test.waitUntilDone();
-    pb.on("start", function onStart(evt) {
+    pb.on("start", function onStart() {
       test.assertEqual(this, pb, "`this` should be private-browsing module");
-      test.assert(!!evt, "Listener should be passed event object");
-      test.assertEqual(evt.emitter, pb,
-                       "event.emitter should be private-browsing module");
       test.assert(pbService.privateBrowsingEnabled,
                   'private mode is active when "start" event is emitted');
       test.assert(pb.isActive,
@@ -90,11 +87,8 @@ if (pbService) {
 
   exports.testStop = function(test) {
     test.waitUntilDone();
-    pb.on("stop", function onStop(evt) {
+    pb.on("stop", function onStop() {
       test.assertEqual(this, pb, "`this` should be private-browsing module");
-      test.assert(!!evt, "Listener should be passed event object");
-      test.assertEqual(evt.emitter, pb,
-                       "event.emitter should be private-browsing module");
       test.assertEqual(pbService.privateBrowsingEnabled, false,
                        "private mode is disabled when stop event is emitted");
       test.assertEqual(pb.isActive, false,
