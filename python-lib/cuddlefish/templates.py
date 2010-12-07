96670ccdd092259ae609e3bd77a14d8aea975996
Myk Melez <myk@mozilla.org>
2010-12-06 16:31:14 -0800

    bug 616926: improve 'cfx init' code formatting and wording of prose output; r=adw (cherry picked from commit 002e5c3d9249ea7fccf310080c9a9a8c35d1f1f8)

--- python-lib/cuddlefish/templates.py
+++ python-lib/cuddlefish/templates.py
@@ -2,8 +2,6 @@
 MAIN_JS = '''\
 const widgets = require("widget");
 const tabs = require("tabs");
-const panels = require("panel");
-const data = require("self").data;
 
 var widget = widgets.Widget({
   label: "Mozilla website",
@@ -13,22 +11,20 @@ var widget = widgets.Widget({
   }
 });
 
-console.log("Jetpack sample addon running.");
-/*Your code is here below*/
-
+console.log("The add-on is running.");
 '''
 
 #Template used by test-main.js
 TEST_MAIN_JS = '''\
 const main = require("main");
 
-exports.test_test_run = function(test){
-    test.pass("Unit test running!");
-}
+exports.test_test_run = function(test) {
+  test.pass("Unit test running!");
+};
 
-exports.test_id = function(test){
-    test.assert(require("self").id.length > 0);
-}
+exports.test_id = function(test) {
+  test.assert(require("self").id.length > 0);
+};
 
 exports.test_url = function(test) {
   require("request").Request({
@@ -41,44 +37,42 @@ exports.test_url = function(test) {
   test.waitUntilDone(20000);
 };
 
-exports.test_open_tab = function(test){
-    const tabs = require("tabs");
-    tabs.open({
-        url : "http://www.mozilla.org",
-        onReady : function(tab){
-            test.assertEqual(tab.url,"http://www.mozilla.org/");
-            test.done();
-        }
-    });
-    test.waitUntilDone(20000);
-}
-
+exports.test_open_tab = function(test) {
+  const tabs = require("tabs");
+  tabs.open({
+    url: "http://www.mozilla.org/",
+    onReady: function(tab) {
+      test.assertEqual(tab.url, "http://www.mozilla.org/");
+      test.done();
+    }
+  });
+  test.waitUntilDone(20000);
+};
 '''
 
 #Template used by main.md
 MAIN_JS_DOC = '''\
-Documentation about main function.
-
+The main module is a program that creates a widget.  When a user clicks on
+the widget, the program loads the mozilla.org website in a new tab.
 '''
 
 #Template used by README.md
 README_DOC = '''\
-This is my %(name)s package. It contains:
-* A main module.
-* A main test.
-* Some meager documentation.
+This is the %(name)s add-on.  It contains:
 
+* A program (lib/main.js).
+* A few tests.
+* Some meager documentation.
 '''
 
 #Template used by package.json
 PACKAGE_JSON = '''\
 {
-    "name":"%(name)s",
-    "fullName":"%(name)s",
-    "description":"This is an example of addon description.",
-    "author":"",
-    "license":"MPL",
-    "version":"0.1"
+  "name": "%(name)s",
+  "fullName": "%(name)s",
+  "description": "a basic add-on",
+  "author": "",
+  "license": "MPL 1.1/GPL 2.0/LGPL 2.1",
+  "version": "0.1"
 }
-
 '''
