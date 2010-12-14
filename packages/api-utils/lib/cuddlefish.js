4bb69343d3a83d24df84937c6afa2743a3170d47
Atul Varma <varmaa@gmail.com>
2010-12-14 15:33:04 -0800

    Fixed a sendMessage -> send bug in cuddlefish.js.

--- packages/api-utils/lib/cuddlefish.js
+++ packages/api-utils/lib/cuddlefish.js
@@ -97,7 +97,7 @@
                var e10s = loader.require("e10s");
                var process = e10s.AddonProcess();
                loader.console.log("starting main in remote process.");
-               process.sendMessage("startMain", options.main);
+               process.send("startMain", options.main);
              }
            };
          } else
