d72f29bcf72baada9f9f57f05cc90ca272edc8af
Will Bamberg <wbamberg@mozilla.com>
2011-01-27 09:06:18 -0800

    Bug 624607 - Annotator example add-on; r=warner

---
+++ examples/annotator/data/editor/annotation-editor.js
@@ -0,0 +1,19 @@
+/*
+On a return key, send the content of the textArea back to the add-on,
+and zero the textArea for the next time.
+*/
+var textArea = document.getElementById('annotation-box');
+textArea.onkeyup = function(event) {
+  if (event.keyCode == 13) {
+    postMessage(textArea.value);
+    textArea.value = '';
+  }
+};
+
+/*
+Assign focus to the textArea.
+*/
+self.on('message', function() {
+  var textArea = document.getElementById('annotation-box');
+  textArea.focus();
+})
