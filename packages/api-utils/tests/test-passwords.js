7a9542b49cd67127817e770ca4d9c9302c0ae0f2
Irakli Gozalishvili <rfobic@gmail.com>
2011-03-14 14:54:34 +0100

    Adding tests for web page associated passwords.

--- packages/api-utils/tests/test-passwords.js
+++ packages/api-utils/tests/test-passwords.js
@@ -49,6 +49,41 @@ exports["test addon associated credentials"] = function(assert) {
 
   remove(search(options)[0]);
   assert.ok(!search(options).length, "remove worked");
+};
+
+exports["test web page associated credentials"] = function(assert) {
+  let options = {
+    url: "http://foo.bar.com",
+    formSubmitURL: "http://login.foo.bar.com",
+    username: "user",
+    password: "pass",
+    usernameField: "user-f",
+    passwordField: "pass-f"
+  };
+  store(options);
+
+  assert.ok(search().length, "credential was stored");
+  assert.ok(search(options).length, "stored credential found");
+  assert.ok(search({ username: options.username }).length, "found by username");
+  assert.ok(search({ password: options.password }).length, "found by password");
+  assert.ok(search({ formSubmitURL: options.formSubmitURL }).length,
+            "found by formSubmitURL");
+  assert.ok(search({ usernameField: options.usernameField }).length,
+            "found by usernameField");
+  assert.ok(search({ passwordField: options.passwordField }).length,
+            "found by passwordField");
+
+  let credential = search(options)[0];
+  assert.equal(credential.url, options.url, "url matches");
+  assert.equal(credential.username, options.username, "username matches");
+  assert.equal(credential.password, options.password, "password matches");
+  assert.equal(credential.realm, null, "realm is ");
+  assert.equal(credential.formSubmitURL, options.formSubmitURL,
+               "`formSubmitURL` matches");
+  assert.equal(credential.usernameField, options.usernameField,
+               "usernameField matches");
+  assert.equal(credential.passwordField, options.passwordField,
+               "passwordField matches");
 
   remove(search(options)[0]);
   assert.ok(!search(options).length, "remove worked");
