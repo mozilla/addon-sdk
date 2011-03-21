4ff5be898a5ccfd136baa6e38c704367d25a8691
Irakli Gozalishvili <rfobic@gmail.com>
2011-03-21 17:43:52 +0100

    Refactoring module to remove additional namespace.

--- packages/api-utils/lib/dom/events.js
+++ packages/api-utils/lib/dom/events.js
@@ -45,116 +45,115 @@ function getInitializerName(category) {
   return "init" + singularify(category);
 }
 
-const events = {
-  /**
-   * Registers an event `listener` on a given `element`, that will be called
-   * when events of specified `type` is dispatched on the `element`.
-   * @param {Element} element
-   *    Dom element to register listener on.
-   * @param {String} type
-   *    A string representing the
-   *    [event type](https://developer.mozilla.org/en/DOM/event.type) to
-   *    listen for.
-   * @param {Function} listener
-   *    Function that is called whenever an event of the specified `type` 
-   *    occurs.
-   * @param {Boolean} capture
-   *    If true, indicates that the user wishes to initiate capture. After
-   *    initiating capture, all events of the specified type will be dispatched
-   *    to the registered listener before being dispatched to any `EventTarget`s
-   *    beneath it in the DOM tree. Events which are bubbling upward through
-   *    the tree will not trigger a listener designated to use capture.
-   *    See [DOM Level 3 Events](http://www.w3.org/TR/DOM-Level-3-Events/#event-flow)
-   *    for a detailed explanation.
-   */
-  on: function on(element, type, listener, capture) {
-    // `capture` defaults to `false`.
-    capture = capture || false;
-    element.addEventListener(type, listener, capture);
-  },
+/**
+ * Registers an event `listener` on a given `element`, that will be called
+ * when events of specified `type` is dispatched on the `element`.
+ * @param {Element} element
+ *    Dom element to register listener on.
+ * @param {String} type
+ *    A string representing the
+ *    [event type](https://developer.mozilla.org/en/DOM/event.type) to
+ *    listen for.
+ * @param {Function} listener
+ *    Function that is called whenever an event of the specified `type` 
+ *    occurs.
+ * @param {Boolean} capture
+ *    If true, indicates that the user wishes to initiate capture. After
+ *    initiating capture, all events of the specified type will be dispatched
+ *    to the registered listener before being dispatched to any `EventTarget`s
+ *    beneath it in the DOM tree. Events which are bubbling upward through
+ *    the tree will not trigger a listener designated to use capture.
+ *    See [DOM Level 3 Events](http://www.w3.org/TR/DOM-Level-3-Events/#event-flow)
+ *    for a detailed explanation.
+ */
+var on = exports.on = function on(element, type, listener, capture) {
+  // `capture` defaults to `false`.
+  capture = capture || false;
+  element.addEventListener(type, listener, capture);
+};
 
-  /**
-   * Registers an event `listener` on a given `element`, that will be called
-   * only once, next time event of specified `type` is dispatched on the
-   * `element`.
-   * @param {Element} element
-   *    Dom element to register listener on.
-   * @param {String} type
-   *    A string representing the
-   *    [event type](https://developer.mozilla.org/en/DOM/event.type) to
-   *    listen for.
-   * @param {Function} listener
-   *    Function that is called whenever an event of the specified `type` 
-   *    occurs.
-   * @param {Boolean} capture
-   *    If true, indicates that the user wishes to initiate capture. After
-   *    initiating capture, all events of the specified type will be dispatched
-   *    to the registered listener before being dispatched to any `EventTarget`s
-   *    beneath it in the DOM tree. Events which are bubbling upward through
-   *    the tree will not trigger a listener designated to use capture.
-   *    See [DOM Level 3 Events](http://www.w3.org/TR/DOM-Level-3-Events/#event-flow)
-   *    for a detailed explanation.
-   */
-  once: function once(element, type, listener, capture) {
-    events.on(element, type, function selfRemovableListener(event) {
-      events.removeListener(element, type, selfRemovableListener, capture);
-      listener.apply(this, arguments);
-    }, capture);
-  },
+/**
+ * Registers an event `listener` on a given `element`, that will be called
+ * only once, next time event of specified `type` is dispatched on the
+ * `element`.
+ * @param {Element} element
+ *    Dom element to register listener on.
+ * @param {String} type
+ *    A string representing the
+ *    [event type](https://developer.mozilla.org/en/DOM/event.type) to
+ *    listen for.
+ * @param {Function} listener
+ *    Function that is called whenever an event of the specified `type` 
+ *    occurs.
+ * @param {Boolean} capture
+ *    If true, indicates that the user wishes to initiate capture. After
+ *    initiating capture, all events of the specified type will be dispatched
+ *    to the registered listener before being dispatched to any `EventTarget`s
+ *    beneath it in the DOM tree. Events which are bubbling upward through
+ *    the tree will not trigger a listener designated to use capture.
+ *    See [DOM Level 3 Events](http://www.w3.org/TR/DOM-Level-3-Events/#event-flow)
+ *    for a detailed explanation.
+ */
+exports.once = function once(element, type, listener, capture) {
+  on(element, type, function selfRemovableListener(event) {
+    removeListener(element, type, selfRemovableListener, capture);
+    listener.apply(this, arguments);
+  }, capture);
+};
 
-  /**
-   * Unregisters an event `listener` on a given `element` for the events of the
-   * specified `type`.
-   *
-   * @param {Element} element
-   *    Dom element to unregister listener from.
-   * @param {String} type
-   *    A string representing the
-   *    [event type](https://developer.mozilla.org/en/DOM/event.type) to
-   *    listen for.
-   * @param {Function} listener
-   *    Function that is called whenever an event of the specified `type` 
-   *    occurs.
-   * @param {Boolean} capture
-   *    If true, indicates that the user wishes to initiate capture. After
-   *    initiating capture, all events of the specified type will be dispatched
-   *    to the registered listener before being dispatched to any `EventTarget`s
-   *    beneath it in the DOM tree. Events which are bubbling upward through
-   *    the tree will not trigger a listener designated to use capture.
-   *    See [DOM Level 3 Events](http://www.w3.org/TR/DOM-Level-3-Events/#event-flow)
-   *    for a detailed explanation.
-   */
-  removeListener: function removeListener(element, type, listener, capture) {
-    element.removeEventListener(type, listener, capture);
-  },
+/**
+ * Unregisters an event `listener` on a given `element` for the events of the
+ * specified `type`.
+ *
+ * @param {Element} element
+ *    Dom element to unregister listener from.
+ * @param {String} type
+ *    A string representing the
+ *    [event type](https://developer.mozilla.org/en/DOM/event.type) to
+ *    listen for.
+ * @param {Function} listener
+ *    Function that is called whenever an event of the specified `type` 
+ *    occurs.
+ * @param {Boolean} capture
+ *    If true, indicates that the user wishes to initiate capture. After
+ *    initiating capture, all events of the specified type will be dispatched
+ *    to the registered listener before being dispatched to any `EventTarget`s
+ *    beneath it in the DOM tree. Events which are bubbling upward through
+ *    the tree will not trigger a listener designated to use capture.
+ *    See [DOM Level 3 Events](http://www.w3.org/TR/DOM-Level-3-Events/#event-flow)
+ *    for a detailed explanation.
+ */
+var removeListener = exports.removeListener =
+    function removeListener(element, type, listener, capture) {
+      element.removeEventListener(type, listener, capture);
+    };
 
-  /**
-   * Emits event of the specified `type` and `category` on the given `element`.
-   * Specified `settings` are used to initialize event before dispatching it.
-   * @param {Element} element
-   *    Dom element to dispatch event on.
-   * @param {String} type
-   *    A string representing the
-   *    [event type](https://developer.mozilla.org/en/DOM/event.type).
-   * @param {Object} options
-   *    Options object containing following properties:
-   *    - `category`: String passed to the `document.createEvent` if not present
-   *      "UIEvents" is used instead.
-   *    - `initializer`: If passed it will be used as name of the method used
-   *      to initialize event. If omitted name will be generated from the
-   *      `category` field by prefixing it with `"init"` and removing last
-   *      character if it matches `"s"`.
-   *    - `settings`: Array of settings that are forwarded to the event
-   *      initializer after firs `type` argument.
-   * @see https://developer.mozilla.org/En/DOM/Document.createEvent
-   */
-  emit: function emit(element, type, { category, initializer, settings }) {
+/**
+ * Emits event of the specified `type` and `category` on the given `element`.
+ * Specified `settings` are used to initialize event before dispatching it.
+ * @param {Element} element
+ *    Dom element to dispatch event on.
+ * @param {String} type
+ *    A string representing the
+ *    [event type](https://developer.mozilla.org/en/DOM/event.type).
+ * @param {Object} options
+ *    Options object containing following properties:
+ *    - `category`: String passed to the `document.createEvent` if not present
+ *      "UIEvents" is used instead.
+ *    - `initializer`: If passed it will be used as name of the method used
+ *      to initialize event. If omitted name will be generated from the
+ *      `category` field by prefixing it with `"init"` and removing last
+ *      character if it matches `"s"`.
+ *    - `settings`: Array of settings that are forwarded to the event
+ *      initializer after firs `type` argument.
+ * @see https://developer.mozilla.org/En/DOM/Document.createEvent
+ */
+exports.emit =
+  function emit(element, type, { category, initializer, settings }) {
     category = category || "UIEvents";
     initializer = initializer || getInitializerName(category);
     let document = element.ownerDocument;
     let event = document.createEvent(category);
     event[initializer].apply(event, [type].concat(settings));
     element.dispatchEvent(event);
-  }
-};
-exports.events = events;
+  };
