c84d03bcae7c9ee775086f1769def93afe467268
Wes Kocher <kwierso@gmail.com>
2011-03-27 23:20:34 -0700

    bug 619547: provide way to programmatically determine package name; r=myk

--- packages/api-utils/lib/self.js
+++ packages/api-utils/lib/self.js
@@ -7,6 +7,7 @@ let packageData = packaging.options.packageData;
 let resourcePackages = packaging.options.resourcePackages;
 let id = packaging.jetpackID;
 exports.id = id;
+exports.name = packaging.options.name;
 
 // Some XPCOM APIs require valid URIs as an argument for certain operations (see
 // `nsILoginManager` for example). This property represents add-on associated
