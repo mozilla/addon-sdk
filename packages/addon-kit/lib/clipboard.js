6a46817f895a3a9927f9e4c23568b4c40cca613e
Erik Vold <erikvvold@gmail.com>
2011-06-06 22:40:16 -0700

    adding myself as a contributor to the 'clipboard' module

--- packages/addon-kit/lib/clipboard.js
+++ packages/addon-kit/lib/clipboard.js
@@ -23,6 +23,7 @@
  * Contributor(s):
  *   Paul O’Shannessy <paul@oshannessy.com> (Original Author)
  *   Dietrich Ayala <dietrich@mozilla.com>
+ *   Erik Vold <erikvvold@gmail.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
