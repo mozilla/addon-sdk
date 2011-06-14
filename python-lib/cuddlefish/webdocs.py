4cbee6fe95466aed6a6d7a12f9de190dbe26f54f
Brian Warner <warner@mozilla.com>
2011-06-14 14:24:07 -0700

    Bug 664001: bring 'self' back onto 'cfx docs' page. r+a=myk
    Quick hack to bring back docs for the 'self' module. They disappeared when
    self.js was replaced with self-maker.js, since webdocs.py builds the
    documented-modules list by finding .js files with corresponding .md files.
    self.md explains how to use require("self") even though there is no actual
    self.js file ('self' is magical, implemented with code from self-maker.js).
    
    The correct long-term fix will be to change webdocs.py to look for all .md
    files, rather than looking for .js files and then testing to see if there's a
    similarly-named .md file in the same package. But this is enough to get
    'self' back into the docs for 1.0.
    (cherry picked from commit 262117fe2937b81a06bdec731b44ad8411d2dc94)

--- python-lib/cuddlefish/webdocs.py
+++ python-lib/cuddlefish/webdocs.py
@@ -41,6 +41,11 @@ def get_documented_modules(root, package_name, modules_json):
         path = os.path.join(*module)
         if module_md_exists(module_md_root, path):
             documented_modules.append(module)
+    if package_name == "addon-kit":
+        # hack for bug 664001, self-maker.js is in api-utils, self.md is in
+        # addon-kit. Real fix is for this function to look for all .md files,
+        # not for .js files with matching .md file in the same package.
+        documented_modules.append(["self"])
     return doc_prefix, documented_modules
 
 def module_md_exists(root, module_name):
