3f58f2692c99a9844c4f7de868a383a165ea1a01
Erik Vold <erikvvold@gmail.com>
2011-06-30 09:20:45 -0700

    bug fix: a loading window could fail to unregister as of d3bff6344bc128b30cee21f7893a4c288f00b6f2 (note: needs a test..)

--- packages/api-utils/lib/window-utils.js
+++ packages/api-utils/lib/window-utils.js
@@ -103,13 +103,14 @@ WindowTracker.prototype = {
   },
 
   _unregWindow: function _unregWindow(window) {
-    if (!this.delegate.onUntrack)
-      return;
+    if (window.document.readyState == "complete") {
+      if (!this.delegate.onUntrack)
+        return;
 
-    if (window.document.readyState == "complete")
       this.delegate.onUntrack(window);
-    else
+    } else {
       this._unregLoadingWindow(window);
+    }
   },
 
   unload: function unload() {
