c06c389ba31c06657f778fc5d46e235f86220b33
Irakli Gozalishvili <rfobic@gmail.com>
2011-07-26 14:39:12 +0200

    Changing test that relied on mutability of exports.

--- packages/api-utils/tests/interoperablejs-read-only/compliance/monkeys/program.js
+++ packages/api-utils/tests/interoperablejs-read-only/compliance/monkeys/program.js
@@ -1,5 +1,5 @@
 var b = require('b');
 var a = require('a');
 var test = require('test');
-test.assert(b.monkey == 10, 'monkeys permitted');
+test.assert(b.monkey != 10, 'monkeys not permitted on exports');
 test.print('DONE', 'info');
