544ab803ef894978a9d31a4418a5bd6397cc070e
Sander van Veen <sandervv@gmail.com>
2011-09-02 10:38:14 -0700

    bug 680821: report module URLs in about:memory; r=myk

--- packages/api-utils/lib/securable-module.js
+++ packages/api-utils/lib/securable-module.js
@@ -136,7 +136,7 @@
                                         this._defaultPrincipal);
 
        return {
-         _sandbox: new Cu.Sandbox(principal),
+         _sandbox: new Cu.Sandbox(principal, { sandboxName: options.filename }),
          _principal: principal,
          get globalScope() {
            return this._sandbox;
