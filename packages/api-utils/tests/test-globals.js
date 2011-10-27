6e2baac6ed76df1de86b21e224b6687d201bc8e6
Brian Warner <warner@mozilla.com>
2011-10-27 15:12:12 -0700

    apply myk's suggested fixes

--- packages/api-utils/tests/test-globals.js
+++ packages/api-utils/tests/test-globals.js
@@ -1,18 +1,21 @@
+Object.defineProperty(this, "global", { value: this });
+
 exports.testGlobals = function(test) {
   // the only globals in module scope should be:
   //   module, exports, require, dump, console
-  test.assertEqual(typeof(module), "object", "have 'module', good");
-  test.assertEqual(typeof(exports), "object", "have 'exports', good");
-  test.assertEqual(typeof(require), "function", "have 'require', good");
-  test.assertEqual(typeof(dump), "function", "have 'dump', good");
-  test.assertEqual(typeof(console), "object", "have 'console', good");
+  test.assertObject(module, "have 'module', good");
+  test.assertObject(exports, "have 'exports', good");
+  test.assertFunction(require, "have 'require', good");
+  test.assertFunction(dump, "have 'dump', good");
+  test.assertObject(console, "have 'console', good");
 
   // in particular, these old globals should no longer be present
-  test.assertEqual(typeof(packaging), "undefined", "no 'packaging', good");
-  //test.assertEqual(typeof(memory), "undefined", "no 'memory', good"); // bug 620559
+  test.assertUndefined(global.packaging, "no 'packaging', good");
+  test.expectFail(
+    // this will be fixed by bug 620559
+    function () test.assertUndefined(global.memory, "no 'memory', good")
+  );
 
   test.assertMatches(module.uri, /test-globals\.js$/,
                      'should contain filename');
-
-  test.assertEqual(typeof memory, 'object', 'should define memory');
 };
