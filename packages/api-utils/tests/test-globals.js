d99b8b5ac89790578ebf831f8ad36c01a6972272
Brian Warner <warner@mozilla.com>
2011-10-27 11:16:42 -0700

    Bug 596077: test that 'packaging' is really gone (new loader omits it)
    The new loader (yay!) finally removed "packaging" from the globals available
    to each module (in favor of require("@packaging")). All we need to do now is
    to test that it's really missing.
    
    'memory' is still present, but that will go away soon too (bug 620559)

--- packages/api-utils/tests/test-globals.js
+++ packages/api-utils/tests/test-globals.js
@@ -1,7 +1,18 @@
 exports.testGlobals = function(test) {
+  // the only globals in module scope should be:
+  //   module, exports, require, dump, console
+  test.assertEqual(typeof(module), "object", "have 'module', good");
+  test.assertEqual(typeof(exports), "object", "have 'exports', good");
+  test.assertEqual(typeof(require), "function", "have 'require', good");
+  test.assertEqual(typeof(dump), "function", "have 'dump', good");
+  test.assertEqual(typeof(console), "object", "have 'console', good");
+
+  // in particular, these old globals should no longer be present
+  test.assertEqual(typeof(packaging), "undefined", "no 'packaging', good");
+  //test.assertEqual(typeof(memory), "undefined", "no 'memory', good"); // bug 620559
+
   test.assertMatches(module.uri, /test-globals\.js$/,
                      'should contain filename');
 
-  test.assertEqual(typeof console, 'object', 'should define console');
   test.assertEqual(typeof memory, 'object', 'should define memory');
 };
