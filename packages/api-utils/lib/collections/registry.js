57e59bd70d905995c96dfaa2e37674bc4022c68a
Irakli Gozalishvili <rfobic@gmail.com>
2011-11-17 18:21:46 -0800

    Implement registry abstraction.

---
+++ packages/api-utils/lib/collections/registry.js
@@ -0,0 +1,67 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Jetpack.
+ *
+ * The Initial Developer of the Original Code is Mozilla.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Irakli Gozalishvili <gozala@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+const { map, has, get, set, unset  } = require('./map');
+const { uuid } = require('../uuid');
+
+/**
+ * Initialize registry.
+ */
+exports.registry = map;
+
+/**
+ * Unregister element with a given id.
+ */
+exports.unregister = unset;
+
+/**
+ * Get element registered with the given id.
+ */
+exports.get = get;
+
+/**
+ * Returns true if there is an element registered with a given `id`.
+ */
+exports.has = has;
+
+/**
+ * Register given `element`.
+ */
+exports.register = function register(source, element) {
+  let id = String(uuid());
+  set(source, id, element);
+  return id;
+};
