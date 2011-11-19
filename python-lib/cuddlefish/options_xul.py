c163a67884b0d2fa8d63df8c272486de7969b863
Erik Vold <erikvvold@gmail.com>
2011-11-19 08:49:26 -0800

    adding some validation for options_xul.py (but I'm not sure if this is a good place to be raising errors..)

--- python-lib/cuddlefish/options_xul.py
+++ python-lib/cuddlefish/options_xul.py
@@ -1,6 +1,38 @@
 from xml.dom.minidom import Document
 
+VALID_PREF_TYPES = ['bool', 'boolint', 'integer', 'string', 'color', 'file',
+                    'directory', 'control']
+
+class Error(Exception):
+    pass
+
+class BadPrefTypeError(Error):
+    pass
+
+class MissingPrefAttr(Error):
+    pass
+
+def validate_prefs(options):
+    for pref_name in options:
+        pref = options[pref_name]
+
+        # Make sure there is a 'title'
+        if ("title" not in pref):
+            raise MissingPrefAttr("The '%s' pref requires a 'title'" % (pref_name))
+
+        # Make sure that the pref type is a valid inline pref type
+        if (pref["type"] not in VALID_PREF_TYPES):
+            raise BadPrefTypeError('%s is not a valid inline pref type' % (pref["type"]))
+
+        # Make sure the 'control' type has a 'label'
+        if (pref["type"] == "control"):
+            if ("label" not in pref):
+                raise MissingPrefAttr("The 'control' inline pref type requires a 'label'")
+
+        # TODO: Check that pref["type"] matches default value type
+
 def parse_options(options, jetpack_id):
+    validate_prefs(options)
 
     doc = Document()
     root = doc.createElement("vbox")
@@ -14,20 +46,19 @@ def parse_options(options, jetpack_id):
         setting.setAttribute("type", pref["type"])
         setting.setAttribute("title", pref["title"])
 
-        if (pref["type"] == "button"):
-            setting.setAttribute("type", "control")
+        if ("description" in pref):
+            setting.appendChild(doc.createTextNode(pref["description"]))
+
+        if (pref["type"] == "control"):
             button = doc.createElement("button")
             button.setAttribute("label", pref["label"])
-            button.setAttribute("oncommand","Services.obs.notifyObservers(null, '"+
-                                              jetpack_id+"-cmdPressed', '"+
-                                              pref_name+"');");
+            button.setAttribute("oncommand", "Services.obs.notifyObservers(null, '" +
+                                              jetpack_id + "-cmdPressed', '" +
+                                              pref_name + "');");
             setting.appendChild(button)
         elif (pref["type"] == "boolint"):
             setting.setAttribute("on", pref["on"])
             setting.setAttribute("off", pref["off"])
-
-        if ("description" in pref):
-            setting.appendChild(doc.createTextNode(pref["description"]))
 
         root.appendChild(setting)
 
