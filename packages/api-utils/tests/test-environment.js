f9ae07250f07d7d2124cae3fa84ad0bf9b0f7da0
Irakli Gozalishvili <rfobic@gmail.com>
2011-11-23 16:40:21 -0800

    Use less likely used names for env variables.

--- packages/api-utils/tests/test-environment.js
+++ packages/api-utils/tests/test-environment.js
@@ -8,39 +8,39 @@ const { get, set, exists } = Cc['@mozilla.org/process/environment;1'].
 exports['test exists'] = function(assert) {
   assert.equal('PATH' in env, exists('PATH'),
                'PATH environment variable is defined');
-  assert.equal('FOO' in env, exists('FOO'),
-               'FOO environment variable is not defined');
-  set('FOO', 'foo');
-  assert.equal('FOO' in env, true,
-               'FOO environment variable was set');
-  set('FOO', null);
-  assert.equal('FOO' in env, false,
-               'FOO environment variable was unset');
+  assert.equal('FOO1' in env, exists('FOO1'),
+               'FOO1 environment variable is not defined');
+  set('FOO1', 'foo');
+  assert.equal('FOO1' in env, true,
+               'FOO1 environment variable was set');
+  set('FOO1', null);
+  assert.equal('FOO1' in env, false,
+               'FOO1 environment variable was unset');
 };
 
 exports['test get'] = function(assert) {
   assert.equal(env.PATH, get('PATH'), 'PATH env variable matches');
-  assert.equal(env.BAR, undefined, 'BAR env variable is not defined');
-  set('BAR', 'bar');
-  assert.equal(env.BAR, 'bar', 'BAR env variable was set');
-  set('BAR', null);
-  assert.equal(env.BAR, undefined, 'BAR env variable was unset');
+  assert.equal(env.BAR2, undefined, 'BAR2 env variable is not defined');
+  set('BAR2', 'bar');
+  assert.equal(env.BAR2, 'bar', 'BAR2 env variable was set');
+  set('BAR2', null);
+  assert.equal(env.BAR2, undefined, 'BAR2 env variable was unset');
 };
 
 exports['test set'] = function(assert) {
-  assert.equal(get('BAZ'), '', 'BAZ env variable is not set');
-  assert.equal(env.BAZ, undefined, 'BAZ is not set');
-  env.BAZ = 'baz';
-  assert.equal(env.BAZ, get('BAZ'), 'BAZ env variable is set');
-  assert.equal(get('BAZ'), 'baz', 'BAZ env variable was set to "baz"');
+  assert.equal(get('BAZ3'), '', 'BAZ3 env variable is not set');
+  assert.equal(env.BAZ3, undefined, 'BAZ3 is not set');
+  env.BAZ3 = 'baz';
+  assert.equal(env.BAZ3, get('BAZ3'), 'BAZ3 env variable is set');
+  assert.equal(get('BAZ3'), 'baz', 'BAZ3 env variable was set to "baz"');
 };
 
 exports['test unset'] = function(assert) {
-  env.BLA = 'bla';
-  assert.equal(env.BLA, 'bla', 'BLA env varibale is set');
-  delete env.BLA;
-  assert.equal(env.BLA, undefined, 'BLA env variable is unset');
-  assert.equal('BLA' in env, false, 'BLA env variable no longer exists' );
+  env.BLA4 = 'bla';
+  assert.equal(env.BLA4, 'bla', 'BLA4 env varibale is set');
+  delete env.BLA4;
+  assert.equal(env.BLA4, undefined, 'BLA4 env variable is unset');
+  assert.equal('BLA4' in env, false, 'BLA4 env variable no longer exists' );
 };
 
 require('test').run(exports);
