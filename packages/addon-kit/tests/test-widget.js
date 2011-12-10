d5a8d2fb30eb91b9be6ed69fec2eb02a2c17148f
Erik Vold <erikvvold@gmail.com>
2011-12-10 15:32:20 -0800

    adding test for bug 652527 which was resolved in ca272b37d

--- packages/addon-kit/tests/test-widget.js
+++ packages/addon-kit/tests/test-widget.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const {Cc,Ci} = require("chrome");
 const { Loader } = require('./helpers');
 
@@ -86,7 +88,7 @@ exports.testConstructor = function(test) {
     function() widgets.Widget({id:"fooID", label: "foo", content: "", image: ""}),
     "No content or contentURL property found. Widgets must have one or the other.",
     "throws on empty content");
-  
+
   // Test duplicated ID
   let duplicateID = widgets.Widget({id: "foo", label: "foo", content: "bar"});
   test.assertRaises(
@@ -94,7 +96,13 @@ exports.testConstructor = function(test) {
     /This widget ID is already used:/,
     "throws on duplicated id");
   duplicateID.destroy();
-  
+
+  // Test Bug 652527
+  test.assertRaises(
+    function() widgets.Widget({id: "", label: "bar", content: "bar"}),
+    /You have to specify a unique value for the id property of/,
+    "throws on falsey id");
+
   // Test duplicate label, different ID
   let w1 = widgets.Widget({id: "id1", label: "foo", content: "bar"});
   let w2 = widgets.Widget({id: "id2", label: "foo", content: "bar"});
