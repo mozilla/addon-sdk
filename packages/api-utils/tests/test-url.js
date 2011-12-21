45859d658a1283e2a5e94940ab100285a6d94056
Irakli Gozalishvili <rfobic@gmail.com>
2011-12-21 12:12:06 -0800

    Remove `toFilename` and `fromFilename` functions as they were only useful with unpacked add-ons.

--- packages/api-utils/tests/test-url.js
+++ packages/api-utils/tests/test-url.js
@@ -74,50 +74,6 @@ exports.testParseFTPWithUserPass = function(test) {
   test.assertEqual(info.userPass, "user:pass");
 };
 
-exports.testToFilename = function(test) {
-  test.assertRaises(
-    function() { url.toFilename("resource://nonexistent"); },
-    "resource does not exist: resource://nonexistent/",
-    "url.toFilename() on nonexistent resources should throw"
-  );
-
-  test.assertMatches(url.toFilename(module.uri),
-                     /.*test-url\.js$/,
-                     "url.toFilename() on resource: URIs should work");
-
-  test.assertRaises(
-    function() { url.toFilename("http://foo.com/"); },
-    "cannot map to filename: http://foo.com/",
-    "url.toFilename() on http: URIs should raise error"
-  );
-
-  try {
-    test.assertMatches(
-      url.toFilename("chrome://global/content/console.xul"),
-      /.*console\.xul$/,
-      "url.toFilename() w/ console.xul works when it maps to filesystem"
-    );
-  } catch (e) {
-    if (/chrome url isn\'t on filesystem/.test(e.message))
-      test.pass("accessing console.xul in jar raises exception");
-    else
-      test.fail("accessing console.xul raises " + e);
-  }
-
-  // TODO: Are there any chrome URLs that we're certain exist on the
-  // filesystem?
-  // test.assertMatches(url.toFilename("chrome://myapp/content/main.js"),
-  //                    /.*main\.js$/);
-};
-
-exports.testFromFilename = function(test) {
-  var fileUrl = url.fromFilename(url.toFilename(module.uri));
-  test.assertEqual(url.URL(fileUrl).scheme, 'file',
-                   'url.toFilename() should return a file: url');
-  test.assertEqual(url.fromFilename(url.toFilename(fileUrl)),
-                   fileUrl);
-};
-
 exports.testURL = function(test) {
   let URL = url.URL;
   test.assert(URL("h:foo") instanceof URL, "instance is of correct type");
