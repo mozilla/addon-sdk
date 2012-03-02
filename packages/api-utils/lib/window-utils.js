ad6fadb920d41c0598989a56ca2583cde949a6d9
Irakli Gozalishvili <rfobic@gmail.com>
2012-03-02 00:43:34 -0800

    Disable `-moz-binding` only on remote browsers, and configure `docShell` only on local ones.

--- packages/api-utils/lib/window-utils.js
+++ packages/api-utils/lib/window-utils.js
@@ -356,10 +356,13 @@ exports.newTopWindow = newTopWindow;
  * Creates an iframe in a provided document.
  * @params {nsIDOMDocument} document
  * @params {String} options.type
- *    By default is 'content-targetable' for possible values see:
+ *    By default is 'content' for possible values see:
  *    https://developer.mozilla.org/en/XUL/iframe#a-browser.type
  * @params {String} options.uri
  *    URI of the document to be loaded into created frame.
+ * @params {Boolean} options.remote
+ *    If `true` separate process will be used for this frame, also in such
+ *    case all the following options are ignored.
  * @params {Boolean} options.allowAuth
  *    Whether to allow auth dialogs. Defaults to `false`.
  * @params {Boolean} options.allowJavascript
@@ -368,31 +371,35 @@ exports.newTopWindow = newTopWindow;
  *    Whether to allow plugin execution. Defaults to `false`.
  */
 function newFrame(document, options) {
-  let frame, docShell;
-
+  let frame, docShell, remote;
   options = options || {};
+  remote = options.remote === true
+
   frame = document.createElementNS(XUL, 'browser');
   // Type="content" is mandatory to enable stuff here:
   // http://mxr.mozilla.org/mozilla-central/source/content/base/src/nsFrameLoader.cpp#1776
   frame.setAttribute('type', options.type || 'content');
-  // We remove XBL binding to avoid execution of code that is not going to work
-  // because browser has no docShell attribute in remote mode (for example)
-  frame.setAttribute('style','-moz-binding: none;');
+  frame.setAttribute('src', options.uri || 'about:blank');
+
   // Load in separate process if `options.remote` is `true`.
   // http://mxr.mozilla.org/mozilla-central/source/content/base/src/nsFrameLoader.cpp#1347
-  if (options.remote === true)
+  if (remote) {
+    // We remove XBL binding to avoid execution of code that is not going to
+    // work because browser has no docShell attribute in remote mode
+    // (for example)
+    frame.setAttribute('style', '-moz-binding: none;');
     frame.setAttribute('remote', 'true');
+  }
 
   document.documentElement.appendChild(frame);
 
-  docShell = frame.docShell;
-  docShell.allowAuth = options.allowAuth || false;
-  docShell.allowJavascript = options.allowJavascript || false;
-  docShell.allowPlugins = options.allowPlugins || false;
-
-  // If `uri` is provided load it into this frame.
-  if (options.uri)
-    frame.setAttribute('src', options.uri);
+  // If browser is remote it won't have a `docShell`.
+  if (!remote) {
+    docShell = frame.docShell;
+    docShell.allowAuth = options.allowAuth || false;
+    docShell.allowJavascript = options.allowJavascript || false;
+    docShell.allowPlugins = options.allowPlugins || false;
+  }
 
   return frame;
 }
