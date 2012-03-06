718475271de41f3b2baa2496bcea8a2bc738f35c
ZER0 <zer0.kaos@gmail.com>
2012-03-06 07:26:29 +0100

    Updated Fennec Max Version

--- python-lib/cuddlefish/rdf.py
+++ python-lib/cuddlefish/rdf.py
@@ -164,7 +164,7 @@ def gen_manifest(template_root_dir, target_cfg, jid,
         ta_desc.appendChild(elem)
 
         elem = dom.createElement("em:maxVersion")
-        elem.appendChild(dom.createTextNode("11.0a1"))
+        elem.appendChild(dom.createTextNode("13.0a1"))
         ta_desc.appendChild(elem)
 
     if target_cfg.get("homepage"):
