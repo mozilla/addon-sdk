9bddccd8b569ca6011a94a6c48e2854db3be4d77
Alexandre Poirot <poirot.alex@gmail.com>
2012-03-12 19:20:18 +0100

    Bug 739249: Implement HTML Localization.

--- packages/addon-kit/lib/l10n.js
+++ packages/addon-kit/lib/l10n.js
@@ -3,25 +3,21 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-const { Cc, Ci } = require("chrome");
-const { getPreferedLocales, findClosestLocale } = require("api-utils/l10n/locale");
+const core = require("api-utils/l10n/core");
 const { getRulesForLocale } = require("api-utils/l10n/plural-rules");
 
-// Get URI for the addon root folder:
-const { rootURI } = require("@packaging");
-
-let globalHash = {};
-let pluralMappingFunction = getRulesForLocale("en");
+// Retrieve the plural mapping function
+let pluralMappingFunction = getRulesForLocale(core.language()) ||
+                            getRulesForLocale("en");
 
 exports.get = function get(k) {
-
   // For now, we only accept a "string" as first argument
   // TODO: handle plural forms in gettext pattern
   if (typeof k !== "string")
     throw new Error("First argument of localization method should be a string");
 
   // Get translation from big hashmap or default to hard coded string:
-  let localized = globalHash[k] || k;
+  let localized = core.get(k) || k;
 
   // # Simplest usecase:
   //   // String hard coded in source code:
@@ -81,69 +77,3 @@ exports.get = function get(k) {
 
   return localized;
 }
-
-function readURI(uri) {
-  let request = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].
-                createInstance(Ci.nsIXMLHttpRequest);
-  request.open('GET', uri, false);
-  request.overrideMimeType('text/plain');
-  request.send();
-  return request.responseText;
-}
-
-function readJsonUri(uri) {
-  try {
-    return JSON.parse(readURI(uri));
-  }
-  catch(e) {
-    console.error("Error while reading locale file:\n" + uri + "\n" + e);
-  }
-  return {};
-}
-
-// Returns the array stored in `locales.json` manifest that list available
-// locales files
-function getAvailableLocales() {
-  let uri = rootURI + "locales.json";
-  let manifest = readJsonUri(uri);
-
-  return "locales" in manifest && Array.isArray(manifest.locales) ?
-         manifest.locales : [];
-}
-
-// Returns URI of the best locales file to use from the XPI
-function getBestLocaleFile() {
-
-  // Read localization manifest file that contains list of available languages
-  let availableLocales = getAvailableLocales();
-
-  // Retrieve list of prefered locales to use
-  let preferedLocales = getPreferedLocales();
-
-  // Compute the most preferable locale to use by using these two lists
-  let bestMatchingLocale = findClosestLocale(availableLocales, preferedLocales);
-
-  // It may be null if the addon doesn't have any locale file
-  if (!bestMatchingLocale)
-    return null;
-
-  // Retrieve the related plural mapping function
-  let shortLocaleCode = bestMatchingLocale.split("-")[0].toLowerCase();
-  pluralMappingFunction = getRulesForLocale(shortLocaleCode);
-
-  return rootURI + "locale/" + bestMatchingLocale + ".json";
-}
-
-function init() {
-  // First, search for a locale file:
-  let localeURI = getBestLocaleFile();
-  if (!localeURI)
-    return;
-
-  // Locale files only contains one big JSON object that is used as
-  // an hashtable of: "key to translate" => "translated key"
-  // TODO: We are likely to change this in order to be able to overload
-  //       a specific key translation. For a specific package, module or line?
-  globalHash = readJsonUri(localeURI);
-}
-init();
