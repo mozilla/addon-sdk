c052eb2942deecfbd122e0c7e5bb6b9c8a4d278a
ZER0 <zer0.kaos@gmail.com>
2012-04-02 16:24:24 +0200

    Updated base64 image for Unit Test

--- packages/addon-kit/tests/test-clipboard.js
+++ packages/addon-kit/tests/test-clipboard.js
@@ -74,7 +74,7 @@ exports.testSetImage = function(test) {
   var flavor = "image";
   var fullFlavor = "image/png";
 
-  var base64Data = "iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAQ0lEQVRYhe3OwQkAIBTD0Oyqg7idbqUr9B9EhBRyLY8F+0akEyBAgIBvAI1eCuaIEiBAgAABzwH50sNqAgQIEPAYcABJQw5EXdmcNgAAAABJRU5ErkJggg==";
+  var base64Data = "iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAASUlEQVRYhe3O0QkAIAwD0eyqe3Q993AQ3cBSUKpygfsNTyN5ugbQpK0BAADgP0BRDWXWlwEAAAAAgPsA3rzDaAAAAHgPcGrpgAnzQ2FGbWRR9AAAAABJRU5ErkJggg==";
   var contents = "data:image/png;base64," + encodeURIComponent(base64Data);
   test.assert(clip.set(contents, flavor), "clipboard set");
   test.assertEqual(clip.currentFlavors[0], flavor, "flavor is set");
