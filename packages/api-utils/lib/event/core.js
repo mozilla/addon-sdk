9588dc70b23f5d5bbb6b926cb87a5985c2cab6cc
Irakli Gozalishvili <rfobic@gmail.com>
2012-05-07 13:07:35 -0700

    use more obvious form of for each iteration.

--- packages/api-utils/lib/event/core.js
+++ packages/api-utils/lib/event/core.js
@@ -76,8 +76,9 @@ exports.once = once;
  *    More arguments that will be passed to listeners.
  */
 function emit(target, type, message /*, ...*/) {
-  // We just iterate, iterator take care of emitting events.
-  for each (let item in emit.lazy.apply(emit.lazy, arguments));
+  for each (let item in emit.lazy.apply(emit.lazy, arguments)) {
+    // We just iterate, iterator take care of emitting events.
+  }
 }
 
 /**
