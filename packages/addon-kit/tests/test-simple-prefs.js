6d8d47cc4acdb4376c3e400607b51b36994e032d
Erik Vold <erikvvold@gmail.com>
2012-06-02 14:49:30 -0700

    use strict

--- packages/addon-kit/tests/test-simple-prefs.js
+++ packages/addon-kit/tests/test-simple-prefs.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+"use strict";
 
 const { Loader } = require("test-harness/loader");
 const { setTimeout } = require("timers");
