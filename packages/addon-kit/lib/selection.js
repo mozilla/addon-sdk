de5817464b4bb5472216aa648a944832aaf6a1d0
ZER0 <zer0.kaos@gmail.com>
2012-07-03 00:46:34 +0200

    Observes the `document-shown` topic to add again the selection's listeners

--- packages/addon-kit/lib/selection.js
+++ packages/addon-kit/lib/selection.js
@@ -17,12 +17,49 @@ let { Ci, Cc } = require("chrome"),
     { emit, off } = require("api-utils/event/core"),
     { Unknown } = require("api-utils/xpcom"),
     { Class, obscure } = require("api-utils/heritage"),
-    { EventTarget } = require("api-utils/event/target");
+    { EventTarget } = require("api-utils/event/target"),
+    { observers = require("api-utils/observer-service"),
+    { ns } = require("api-utils/namespace");
 
+// When a document is not visible anymore the selection object is detached, and
+// a new selection object is created when it becomes visible again.
+// That makes the previous selection's listeners added previously totally 
+// useless – the listeners are not notified anymore.
+// To fix that we're listening for `document-shown` event in order to add
+// the listeners to the new selection object created.
+//
+// See bug 665386 for further details.
+
+let selections = ns();
+
+observers.add("document-shown", function (document) {
+  var window = document.defaultView;
+
+  let selection = selections(window).selection;
+
+  // We want to handle only the windows where we added selection's listeners 
+  if (selection) {
+    let currentSelection = window.getSelection();
+
+    // If the current selection for the window given is different from the one
+    // stored in the namespace, we need to add the listeners again, and replace
+    // the previous selection in our list with the new one.
+    //
+    // Notice that we don't have to remove the listeners from the old selection,
+    // because is detached. An attempt to remove the listener, will raise an
+    // error (see http://mxr.mozilla.org/mozilla-central/source/layout/generic/nsSelection.cpp#5343 )
+
+    if (currentSelection instanceof Ci.nsISelectionPrivate && 
+      currentSelection !== selection) {
+
+      currentSelection.addSelectionListener(SelectionListenerManager);
+      selections(window).selection = currentSelection;
+    }
+  }  
+});
 
 const windowMediator = Cc["@mozilla.org/appshell/window-mediator;1"].
                        getService(Ci.nsIWindowMediator);
-
 
 // The selection type HTML
 const HTML = 0x01;
@@ -342,8 +379,11 @@ let SelectionListenerManager = Class({
     if (window.jetpack_core_selection_listener)
       return;
     let selection = window.getSelection();
-    if (selection instanceof Ci.nsISelectionPrivate)
+    if (selection instanceof Ci.nsISelectionPrivate) {
       selection.addSelectionListener(this);
+
+      selections(window).selection = selection;
+    }
 
     // nsISelectionListener implementation seems not fire a notification if
     // a selection is in a text field, therefore we need to add a listener to
@@ -367,8 +407,11 @@ let SelectionListenerManager = Class({
     if (!window.jetpack_core_selection_listener)
       return;
     let selection = window.getSelection();
-    if (selection instanceof Ci.nsISelectionPrivate)
+    if (selection instanceof Ci.nsISelectionPrivate) {
       selection.removeSelectionListener(this);
+
+      selections(window).selection = null;
+    }
 
     window.removeEventListener("select", onSelect);
 
