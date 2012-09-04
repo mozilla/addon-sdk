f82918a77cef710a719ee68ca2a3abf61ab8d88c
Erik Vold <erikvvold@gmail.com>
2012-09-04 15:51:49 -0700

    BrowserWindow now extends EventTarget

--- packages/api-utils/lib/window/browser-window.js
+++ packages/api-utils/lib/window/browser-window.js
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-"use strict";
+'use strict';
 
 const { Class } = require('api-utils/heritage');
 const { windowNS } = require('api-utils/window/namespace');
@@ -11,45 +11,14 @@ const { openDialog } = require('api-utils/window/utils');
 const unload = require('api-utils/unload');
 const { getWindowTitle } = require('api-utils/window/utils');
 const { getMode } = require('api-utils/private-browsing/utils');
+const { EventTarget } = require('api-utils/event/target');
 
 const ERR_FENNEC_MSG = 'This method is not yet supported by Fennec, consider using require("tabs") instead';
 
 const BrowserWindow = Class({
   initialize: function initialize(options) {
-      // make sure we don't have unhandled errors
-      this.on('error', console.exception.bind(console));
-
-      if ('onOpen' in options)
-        this.on('open', options.onOpen);
-      if ('onClose' in options)
-        this.on('close', options.onClose);
-      if ('onActivate' in options)
-        this.on('activate', options.onActivate);
-      if ('onDeactivate' in options)
-        this.on('deactivate', options.onDeactivate);
-
-      if ('tabs' in options) {
-        var tabOptions = Array.isArray(options.tabs) ?
-                                    options.tabs.map(Options) :
-                                    [ Options(options.tabs) ];
-      }
-      else if ('url' in options) {
-        var tabOptions = [ Options(options.url) ];
-      }
-
-      if ('window' in options) {
-        windowNS(this).window = options.window;
-      }
-      else {
-        // TODO: test
-        windowNS(this).window = openDialog({
-          args: tabOptions.map(function(options) {
-            return options.url
-          }).join("|")
-        });
-      }
-
-    return this;
+    EventTarget.prototype.initialize.call(this, options);
+    windowNS(this).window = options.window;
   },
   destroy: function() {
     return null;
