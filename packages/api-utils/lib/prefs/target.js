a61a1a17a52b89159c3dd2824b54a4e7c2e48fcf
Erik Vold <erikvvold@gmail.com>
2012-09-09 21:38:25 -0700

    PrefsTarget() should work

--- packages/api-utils/lib/prefs/target.js
+++ packages/api-utils/lib/prefs/target.js
@@ -1,10 +1,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+'use strict';
 
-"use strict";
-
-const { Cc, Ci } = require("chrome");
+const { Cc, Ci } = require('chrome');
 const { Class } = require('api-utils/heritage');
 const { EventTarget } = require('api-utils/event/target');
 const { Branch } = require("api-utils/preferences-service");
@@ -14,6 +13,7 @@ const { when: unload } = require("api-utils/unload");
 const PrefsTarget = Class({
   extends: EventTarget,
   initialize: function(options) {
+    options = options || {};
     EventTarget.prototype.initialize.call(this, options);
 
     let branchName = options.branchName || '';
