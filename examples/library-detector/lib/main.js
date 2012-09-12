cacbeff84afcaeff0037e092c07e84957edddea0
Alexandre Poirot <poirot.alex@gmail.com>
2012-09-12 14:56:40 +0200

    Bug 790328: Use `addon` in widget/panel documents of library detector.

--- examples/library-detector/lib/main.js
+++ examples/library-detector/lib/main.js
@@ -8,66 +8,26 @@ const data = require('self').data;
 const pageMod = require('page-mod');
 const panel = require('panel');
 
-const htmlContentPreamble =
-                      '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"' +
-                      ' "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' +
-                      ' <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">' +
-                      '   <head>' +
-                      '     <style type="text/css" media="all">' +
-                      '       img {display: inline;}' +
-                      '     </style>' +
-                      '  </head>' +
-                      '  <body>'
-
-const htmlContentPostamble =
-                      '  </body>' +
-                      '</html>'
-
-const icons = {
-  'jQuery' : 'jquery.ico',
-  'jQuery UI' : 'jquery_ui.ico',
-  'MooTools' : 'mootools.png',
-  'YUI' : 'yui.ico',
-  'Closure' : 'closure.ico',
-  'Modernizr': 'modernizr.ico',
-}
-
-const ICON_WIDTH = 32;
-
-function buildIconHtml(imageName, libraryInfo) {
-  return '<img src="' + data.url("icons/" + imageName) + '" title="' + libraryInfo + '">';
-}
-
-function buildWidgetViewContent(libraryList) {
-  widgetContent = htmlContentPreamble;
-  libraryList.forEach(function(library) {
-      widgetContent += buildIconHtml(icons[library.name], library.name + "<br>Version: " + library.version);
-  });
-  widgetContent += htmlContentPostamble;
-  return widgetContent;
-}
+const ICON_WIDTH = 16;
 
 function updateWidgetView(tab) {
   let widgetView = widget.getView(tab.window);
   if (!tab.libraries) {
     tab.libraries = [];
   }
-  widgetView.content = buildWidgetViewContent(tab.libraries);
+  widgetView.port.emit("update", tab.libraries);
   widgetView.width = tab.libraries.length * ICON_WIDTH;
 }
 
 var widget = widgets.Widget({
   id: "library-detector",
   label: "Library Detector",
-  content: "<html></html>",
-  contentScriptFile: data.url("widget.js"),
+  contentURL: data.url("widget.html"),
   panel: panel.Panel({
     width: 240,
     height: 60,
-    contentScript: 'self.on("message", function(libraryInfo) {' +
-                   '  window.document.body.innerHTML = libraryInfo;' +
-                   '});'
-  }),
+    contentURL: data.url("panel.html")
+  })
 });
 
 widget.port.on('setLibraryInfo', function(libraryInfo) {
