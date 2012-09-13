291bc4ced08351b3513804d10951d63cf4165a88
Erik Vold <erikvvold@gmail.com>
2012-09-13 10:16:48 -0700

    using module.exports = for test-tabs

--- packages/addon-kit/tests/test-tabs.js
+++ packages/addon-kit/tests/test-tabs.js
@@ -1,19 +1,11 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-"use strict";
+'use strict';
 
-var tests = {};
 if (require("api-utils/xul-app").is("Firefox")) {
-  tests = require("./tabs/test-firefox-tabs");
+  module.exports = require("./tabs/test-firefox-tabs");
 }
 else if (require("api-utils/xul-app").is("Fennec")) {
-  tests = require("./tabs/test-fennec-tabs");
+  module.exports = require("./tabs/test-fennec-tabs");
 }
-
-for (var test in tests)
-  exports[test] = tests[test];
-
-tests = require("./tabs/test-tabs");
-for (let test in tests)
-  exports[test] = tests[test];
