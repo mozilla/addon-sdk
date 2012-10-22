03afbc6e374f8465d55f23500cce50d67522b000
ZER0 <zer0.kaos@gmail.com>
2012-10-22 15:19:22 +0200

    Updated test-main.js template to generate CommonJS unit test

--- python-lib/cuddlefish/templates.py
+++ python-lib/cuddlefish/templates.py
@@ -6,10 +6,16 @@
 TEST_MAIN_JS = '''\
 var main = require("main");
 
-exports.test_run = function(test) {
-  test.pass("Unit test running!");
+exports["test main"] = function(assert) {
+  assert.pass("Unit test running!");
 };
 
+exports["test main async"] = function(assert, done) {
+  assert.pass("async Unit test running!");
+  done();
+};
+
+require("test").run(exports);
 '''
 
 #Template used by package.json
