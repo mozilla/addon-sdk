80e140bb88efbe22c014bc399503ca7f65c6d79d
Erik Vold <erikvvold@gmail.com>
2012-10-26 15:11:04 -0700

    test improvments for bug 803529

--- test/test-page-mod.js
+++ test/test-page-mod.js
@@ -859,12 +859,15 @@ exports.testPageModcancelTimeout = function(test) {
   })
 }
 
-exports.testBug803529 = function(test) {
+exports.testExistingOnFrames = function(test) {
   test.waitUntilDone();
 
-  let subIFrame = '<iframe src="data:text/html;charset=utf-8,sub frame" />'
-  let iFrame = '<iframe src="data:text/html;charset=utf-8,' + encodeURIComponent(subIFrame) + '" />';
-  let url = 'data:text/html;charset=utf-8,' + encodeURIComponent(iFrame)
+  let subFrameURL = 'data:text/html;charset=utf-8,testExistingOnFrames-sub-frame';
+  let subIFrame = '<iframe src="' + subFrameURL + '" />'
+  let iFrameURL = 'data:text/html;charset=utf-8,' + encodeURIComponent(subIFrame)
+  let iFrame = '<iframe src="' + iFrameURL + '" />';
+  let url = 'data:text/html;charset=utf-8,' + encodeURIComponent(iFrame);
+  let urls = [url, iFrameURL, subFrameURL]
 
   let counter = 0;
   let tab = openTab(getMostRecentBrowserWindow(), url);
@@ -881,13 +884,28 @@ exports.testBug803529 = function(test) {
       attachTo: ["existing", "frame"],
       contentScriptWhen: 'ready',
       onAttach: function(mod) {
-        if (++counter != 2) return;
-        test.pass('page mod attached to iframe');
-        timer.setTimeout(function() {
-          pagemod.destroy();
-          closeTab(tab);
-          test.done();
-        }, 0);
+        // need to ignore urls that are not part of the test, because other
+        // tests are not closing their tabs when they complete..
+        if (urls.indexOf(mod.url) == -1) {
+          return;
+        }
+        if (++counter < 2) {
+          test.assertEqual(iFrameURL, mod.url, '1st attach is for top frame');
+          return;
+        }
+        else if (counter > 2) {
+          test.fail('applied page mod too many times');
+          return;
+        }
+        else {
+          test.assertEqual(subFrameURL, mod.url, '2nd attach is for sub frame');
+          // need timeout because onAttach is called before the constructor returns
+          timer.setTimeout(function() {
+            pagemod.destroy();
+            closeTab(tab);
+            test.done();
+          }, 0);
+        }
       }
     });
   }
