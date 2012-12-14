61a9015ad8f6304f4be90e1b5fa1010cbde5a389
Myk Melez <myk@mozilla.org>
2012-12-14 15:54:21 -0800

    factor out references to require("sdk/self").name

--- test/test-plain-text-console.js
+++ test/test-plain-text-console.js
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const prefs = require("sdk/preferences/service");
-const { id } = require("sdk/self");
+const { id, name } = require("sdk/self");
 
 const ADDON_LOG_LEVEL_PREF = "extensions." + id + ".sdk.console.logLevel";
 const SDK_LOG_LEVEL_PREF = "extensions.sdk.console.logLevel";
@@ -33,45 +33,45 @@ exports.testPlainTextConsole = function(test) {
   test.pass("PlainTextConsole instantiates");
 
   con.log('testing', 1, [2, 3, 4]);
-  test.assertEqual(lastPrint(), "info: " + require("sdk/self").name + ": testing 1 2,3,4\n",
+  test.assertEqual(lastPrint(), "info: " + name + ": testing 1 2,3,4\n",
                    "PlainTextConsole.log() must work.");
 
   con.info('testing', 1, [2, 3, 4]);
-  test.assertEqual(lastPrint(), "info: " + require("sdk/self").name + ": testing 1 2,3,4\n",
+  test.assertEqual(lastPrint(), "info: " + name + ": testing 1 2,3,4\n",
                    "PlainTextConsole.info() must work.");
 
   con.warn('testing', 1, [2, 3, 4]);
-  test.assertEqual(lastPrint(), "warn: " + require("sdk/self").name + ": testing 1 2,3,4\n",
+  test.assertEqual(lastPrint(), "warn: " + name + ": testing 1 2,3,4\n",
                    "PlainTextConsole.warn() must work.");
 
   con.error('testing', 1, [2, 3, 4]);
-  test.assertEqual(lastPrint(), "error: " + require("sdk/self").name + ": testing 1 2,3,4\n",
+  test.assertEqual(lastPrint(), "error: " + name + ": testing 1 2,3,4\n",
                    "PlainTextConsole.error() must work.");
 
   con.debug('testing', 1, [2, 3, 4]);
-  test.assertEqual(lastPrint(), "debug: " + require("sdk/self").name + ": testing 1 2,3,4\n",
+  test.assertEqual(lastPrint(), "debug: " + name + ": testing 1 2,3,4\n",
                    "PlainTextConsole.debug() must work.");
 
   con.log('testing', undefined);
-  test.assertEqual(lastPrint(), "info: " + require("sdk/self").name + ": testing undefined\n",
+  test.assertEqual(lastPrint(), "info: " + name + ": testing undefined\n",
                    "PlainTextConsole.log() must stringify undefined.");
 
   con.log('testing', null);
-  test.assertEqual(lastPrint(), "info: " + require("sdk/self").name + ": testing null\n",
+  test.assertEqual(lastPrint(), "info: " + name + ": testing null\n",
                    "PlainTextConsole.log() must stringify null.");
 
   con.log("testing", { toString: function() "obj.toString()" });
-  test.assertEqual(lastPrint(), "info: " + require("sdk/self").name + ": testing obj.toString()\n",
+  test.assertEqual(lastPrint(), "info: " + name + ": testing obj.toString()\n",
                    "PlainTextConsole.log() must stringify custom toString.");
 
   con.log("testing", { toString: function() { throw "fail!"; } });
-  test.assertEqual(lastPrint(), "info: " + require("sdk/self").name + ": testing <toString() error>\n",
+  test.assertEqual(lastPrint(), "info: " + name + ": testing <toString() error>\n",
                    "PlainTextConsole.log() must stringify custom bad toString.");
 
   con.exception(new Error("blah"));
 
   var tbLines = prints[0].split("\n");
-  test.assertEqual(tbLines[0], "error: " + require("sdk/self").name + ": An exception occurred.");
+  test.assertEqual(tbLines[0], "error: " + name + ": An exception occurred.");
   test.assertEqual(tbLines[1], "Error: blah");
   test.assertEqual(tbLines[2], module.uri + " 71");
   test.assertEqual(tbLines[3], "Traceback (most recent call last):");
@@ -79,13 +79,13 @@ exports.testPlainTextConsole = function(test) {
   prints = [];
   con.trace();
   tbLines = prints[0].split("\n");
-  test.assertEqual(tbLines[0], "info: " + require("sdk/self").name + ": Traceback (most recent call last):");
+  test.assertEqual(tbLines[0], "info: " + name + ": Traceback (most recent call last):");
   test.assertEqual(tbLines[4].trim(), "con.trace();");
 
-  let debugMessage = "debug: " + require("sdk/self").name + ": \n";
-  let infoMessage = "info: " + require("sdk/self").name + ": \n";
-  let warnMessage = "warn: " + require("sdk/self").name + ": \n";
-  let errorMessage = "error: " + require("sdk/self").name + ": \n";
+  let debugMessage = "debug: " + name + ": \n";
+  let infoMessage = "info: " + name + ": \n";
+  let warnMessage = "warn: " + name + ": \n";
+  let errorMessage = "error: " + name + ": \n";
 
   prefs.set(SDK_LOG_LEVEL_PREF, "all");
   con.debug("");
