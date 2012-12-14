9f4e22a95d72d26df17fde111d345b50e076c1a3
Myk Melez <myk@mozilla.org>
2012-12-14 15:34:42 -0800

    add log level tests; add 'all', 'off' levels; make 'warn' message prefix consistent with warn() method and 'warn' log level

--- test/test-plain-text-console.js
+++ test/test-plain-text-console.js
@@ -3,6 +3,15 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const prefs = require("sdk/preferences/service");
+const { id } = require("sdk/self");
+
+const ADDON_LOG_LEVEL_PREF = "extensions." + id + ".sdk.console.logLevel";
+const SDK_LOG_LEVEL_PREF = "extensions.sdk.console.logLevel";
+
+const HAS_ORIGINAL_ADDON_LOG_LEVEL = prefs.has(ADDON_LOG_LEVEL_PREF);
+const ORIGINAL_ADDON_LOG_LEVEL = prefs.get(ADDON_LOG_LEVEL_PREF);
+const HAS_ORIGINAL_SDK_LOG_LEVEL = prefs.has(SDK_LOG_LEVEL_PREF);
+const ORIGINAL_SDK_LOG_LEVEL = prefs.get(SDK_LOG_LEVEL_PREF);
 
 exports.testPlainTextConsole = function(test) {
   var prints = [];
@@ -15,7 +24,7 @@ exports.testPlainTextConsole = function(test) {
     return last;
   }
 
-  prefs.set("extensions.sdk.console.logLevel", "debug");
+  prefs.set(SDK_LOG_LEVEL_PREF, "all");
 
   var Console = require("sdk/console/plain-text").PlainTextConsole;
   var con = new Console(print);
@@ -31,7 +40,7 @@ exports.testPlainTextConsole = function(test) {
                    "PlainTextConsole.info() must work.");
 
   con.warn('testing', 1, [2, 3, 4]);
-  test.assertEqual(lastPrint(), "warning: " + require("sdk/self").name + ": testing 1 2,3,4\n",
+  test.assertEqual(lastPrint(), "warn: " + require("sdk/self").name + ": testing 1 2,3,4\n",
                    "PlainTextConsole.warn() must work.");
 
   con.error('testing', 1, [2, 3, 4]);
@@ -63,7 +72,7 @@ exports.testPlainTextConsole = function(test) {
   var tbLines = prints[0].split("\n");
   test.assertEqual(tbLines[0], "error: " + require("sdk/self").name + ": An exception occurred.");
   test.assertEqual(tbLines[1], "Error: blah");
-  test.assertEqual(tbLines[2], module.uri + " 61");
+  test.assertEqual(tbLines[2], module.uri + " 70");
   test.assertEqual(tbLines[3], "Traceback (most recent call last):");
 
   prints = [];
@@ -71,4 +80,133 @@ exports.testPlainTextConsole = function(test) {
   tbLines = prints[0].split("\n");
   test.assertEqual(tbLines[0], "info: " + require("sdk/self").name + ": Traceback (most recent call last):");
   test.assertEqual(tbLines[4].trim(), "con.trace();");
+
+  let debugMessage = "debug: " + require("sdk/self").name + ": \n";
+  let infoMessage = "info: " + require("sdk/self").name + ": \n";
+  let warnMessage = "warn: " + require("sdk/self").name + ": \n";
+  let errorMessage = "error: " + require("sdk/self").name + ": \n";
+
+  prefs.set(SDK_LOG_LEVEL_PREF, "all");
+  con.debug("");
+  test.assertEqual(lastPrint(), debugMessage,
+                   "when log level is 'all', debug() prints");
+  con.log("");
+  test.assertEqual(lastPrint(), infoMessage,
+                   "when log level is 'all', log() prints");
+  con.info("");
+  test.assertEqual(lastPrint(), infoMessage,
+                   "when log level is 'all', info() prints");
+  con.warn("");
+  test.assertEqual(lastPrint(), warnMessage,
+                   "when log level is 'all', warn() prints");
+  con.error("");
+  test.assertEqual(lastPrint(), errorMessage,
+                   "when log level is 'all', error() prints");
+
+  prefs.set(SDK_LOG_LEVEL_PREF, "debug");
+  con.debug("");
+  test.assertEqual(lastPrint(), debugMessage,
+                   "when log level is 'debug', debug() prints");
+  con.log("");
+  test.assertEqual(lastPrint(), infoMessage,
+                   "when log level is 'debug', log() prints");
+  con.info("");
+  test.assertEqual(lastPrint(), infoMessage,
+                   "when log level is 'debug', info() prints");
+  con.warn("");
+  test.assertEqual(lastPrint(), warnMessage,
+                   "when log level is 'debug', warn() prints");
+  con.error("");
+  test.assertEqual(lastPrint(), errorMessage,
+                   "when log level is 'debug', error() prints");
+
+  prefs.set(SDK_LOG_LEVEL_PREF, "info");
+  con.debug("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'info', debug() doesn't print");
+  con.log("");
+  test.assertEqual(lastPrint(), infoMessage,
+                   "when log level is 'info', log() prints");
+  con.info("");
+  test.assertEqual(lastPrint(), infoMessage,
+                   "when log level is 'info', info() prints");
+  con.warn("");
+  test.assertEqual(lastPrint(), warnMessage,
+                   "when log level is 'info', warn() prints");
+  con.error("");
+  test.assertEqual(lastPrint(), errorMessage,
+                   "when log level is 'info', error() prints");
+
+  prefs.set(SDK_LOG_LEVEL_PREF, "warn");
+  con.debug("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'warn', debug() doesn't print");
+  con.log("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'warn', log() doesn't print");
+  con.info("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'warn', info() doesn't print");
+  con.warn("");
+  test.assertEqual(lastPrint(), warnMessage,
+                   "when log level is 'warn', warn() prints");
+  con.error("");
+  test.assertEqual(lastPrint(), errorMessage,
+                   "when log level is 'warn', error() prints");
+
+  prefs.set(SDK_LOG_LEVEL_PREF, "error");
+  con.debug("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'error', debug() doesn't print");
+  con.log("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'error', log() doesn't print");
+  con.info("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'error', info() doesn't print");
+  con.warn("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'error', warn() doesn't print");
+  con.error("");
+  test.assertEqual(lastPrint(), errorMessage,
+                   "when log level is 'error', error() prints");
+
+  prefs.set(SDK_LOG_LEVEL_PREF, "off");
+  con.debug("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'off', debug() doesn't print");
+  con.log("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'off', log() doesn't print");
+  con.info("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'off', info() doesn't print");
+  con.warn("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'off', warn() doesn't print");
+  con.error("");
+  test.assertEqual(lastPrint(), null,
+                   "when log level is 'off', error() doesn't print");
+
+  prefs.set(SDK_LOG_LEVEL_PREF, "off");
+  prefs.set(ADDON_LOG_LEVEL_PREF, "all");
+  con.debug("");
+  test.assertEqual(lastPrint(), debugMessage,
+                   "addon log level 'all' overrides SDK log level 'off'");
+
+  prefs.set(SDK_LOG_LEVEL_PREF, "all");
+  prefs.set(ADDON_LOG_LEVEL_PREF, "off");
+  con.error("");
+  test.assertEqual(lastPrint(), null,
+                   "addon log level 'off' overrides SDK log level 'all'");
+
+  if (HAS_ORIGINAL_ADDON_LOG_LEVEL)
+    prefs.set(ADDON_LOG_LEVEL_PREF, ORIGINAL_ADDON_LOG_LEVEL);
+  else
+    prefs.reset(ADDON_LOG_LEVEL_PREF);
+
+  if (HAS_ORIGINAL_SDK_LOG_LEVEL)
+    prefs.set(SDK_LOG_LEVEL_PREF, ORIGINAL_SDK_LOG_LEVEL);
+  else
+    prefs.reset(SDK_LOG_LEVEL_PREF);
 };
