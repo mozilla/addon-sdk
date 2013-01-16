35181b1d4516b2af7ae9d3f6bdcc22f68eda7737
Erik Vold <erikvvold@gmail.com>
2013-01-16 15:04:30 -0800

    removing the emulation of the global pb service

--- test/private-browsing/helper.js
+++ test/private-browsing/helper.js
@@ -6,6 +6,9 @@
 let { Cc,Ci } = require('chrome');
 const unload = require("sdk/system/unload");
 const { Loader } = require('sdk/test/loader');
+const { windows: windowsIterator } = require("sdk/window/utils");
+const windows = require("windows").browserWindows;
+
 let { loader } = LoaderWithHookedConsole();
 const pb = loader.require('sdk/private-browsing');
 const pbUtils = loader.require('sdk/private-browsing/utils');
@@ -27,6 +30,33 @@ function LoaderWithHookedConsole() {
     errors: errors
   }
 }
+
+function activate() {
+  if (pbUtils.isGlobalPBEnabled()) {
+    pb.activate();
+  }
+  else if (pbUtils.isWindowPBEnabled()) {
+    windows.open({private: true})
+  }
+}
+exports.activate = activate;
+
+function deactivate(callback) {
+  if (callback)
+    pb.once('stop', callback);
+
+  if (pbUtils.isGlobalPBEnabled()) {
+    pb.deactivate();
+  }
+  else if (pbUtils.isWindowPBEnabled()) {
+    for each (let win in windowsIterator()) {
+      if (pbUtils.isWindowPrivate(win)) {
+        return win.close();
+      }
+    }
+  }
+}
+exports.deactivate = deactivate;
 
 exports.pb = pb;
 exports.pbUtils = pbUtils;
