cd39593025a204cbf70abe705ded274fe5ec7463
ochameau <poirot.alex@gmail.com>
2013-02-19 06:22:43 -0800

    Merge pull request #802 from ochameau/793925-part6
    Bug 793925: part 6 - Simplify manifest path by stripping package name in order to match path of the module from lib folder. r=@gozala
    Conflicts:
    	test/private-browsing/windows.js
    	test/test-private-browsing.js
    (cherry picked from commit 0b7b6cae65480343d402f8e1e76c8a44ed3ac75d)

--- python-lib/cuddlefish/manifest.py
+++ python-lib/cuddlefish/manifest.py
@@ -59,13 +59,21 @@ class ManifestEntry:
 
     def get_path(self):
         name = self.moduleName
+
         if name.endswith(".js"):
             name = name[:-3]
+        items = []
+        # Only add package name for addons, so that system module paths match
+        # the path from the commonjs root directory and also match the loader
+        # mappings.
+        if self.packageName != "addon-sdk":
+            items.append(self.packageName)
+        # And for the same reason, do not append `lib/`.
         if self.sectionName == "tests":
-            path = "%s/%s/%s" % (self.packageName, self.sectionName, name)
-        else:
-            path = "%s/%s" % (self.packageName, name)
-        return path
+            items.append(self.sectionName)
+        items.append(name)
+
+        return "/".join(items)
 
     def get_entry_for_manifest(self):
         entry = { "packageName": self.packageName,
