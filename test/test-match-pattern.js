9a3615a1f57a0477f12bd89c24083f8abc4ab8f9
Jordan Santell <jsantell@mozilla.com>
2013-04-11 13:38:11 -0700

    Add data:* test to match pattern

--- test/test-match-pattern.js
+++ test/test-match-pattern.js
@@ -35,6 +35,8 @@ exports.testMatchPatternTestTrue = function(test) {
   ok(/https:.*zilla.*/, "https://bugzilla.redhat.com/show_bug.cgi?id=569753");
   ok('*.sample.com', 'http://ex.sample.com/foo.html');
   ok('*.amp.le.com', 'http://ex.amp.le.com');
+
+  ok('data:*', 'data:text/html;charset=utf-8,');
 };
 
 exports.testMatchPatternTestFalse = function(test) {
