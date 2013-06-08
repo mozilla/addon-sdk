430856760c0c24c73002c7a94cec9a2a1a5fbe46
Erik Vold <erikvvold@gmail.com>
2013-06-08 13:55:09 -0700

    web-panel wip

--- test/test-sidebar.js
+++ test/test-sidebar.js
@@ -11,35 +11,36 @@ const { open, close, focus, promise: windowPromise } = require('sdk/window/helpe
 const { setTimeout } = require('sdk/timers');
 const { isPrivate } = require('sdk/private-browsing');
 const { data } = require('sdk/self');
+const { makeID } = require('sdk/ui/sidebar/utils');
 
 exports.testSidebarBasicLifeCycle = function(assert, done) {
-  assert.ok(!getMostRecentBrowserWindow().document.getElementById('test'), 'sidebar id DNE');
+  let testName = 'test';
+  assert.ok(!getMostRecentBrowserWindow().document.getElementById(makeID(testName)), 'sidebar id DNE');
   let sidebarXUL = getMostRecentBrowserWindow().document.getElementById('sidebar');
   assert.ok(sidebarXUL, 'sidebar xul element does exist');
   //assert.notEqual(sidebarXUL.getAttribute(''))
   let sidebar = Sidebar({
-    id: 'test',
+    id: testName,
     label: 'test',
     url: 'about:blank'
   });
+
   assert.pass('The Sidebar constructor worked');
-  let ele = getMostRecentBrowserWindow().document.getElementById('test');
+  let ele = getMostRecentBrowserWindow().document.getElementById(makeID(testName));
   assert.ok(ele, 'sidebar element was added');
   assert.ok(!ele.hasAttribute('checked'), 'the sidebar is not displayed');
 
-  sidebar.on('show', function({ target }) {
+  sidebar.on('show', function() {
     assert.pass('the show event was fired');
     assert.equal(isShowing(sidebar), true, 'the sidebar is showing');
-    assert.equal(target, sidebar, 'the target is the sidebar');
     assert.equal(ele.getAttribute('checked'), 'true', 'the sidebar is displayed');
 
-    sidebar.once('hide', function({ target }) {
+    sidebar.once('hide', function() {
       assert.pass('the hide event was fired');
       assert.equal(isShowing(sidebar), false, 'the sidebar is not showing');
-      assert.equal(target, sidebar, 'the target is the sidebar');
 
       sidebar.destroy();
-      assert.ok(!getMostRecentBrowserWindow().document.getElementById('test'), 'sidebar id DNE');
+      assert.ok(!getMostRecentBrowserWindow().document.getElementById(makeID(testName)), 'sidebar id DNE');
       assert.pass('calling destroy worked without error');
 
       done();
@@ -60,16 +61,16 @@ exports.testSideBarIsInNewWindows = function(assert, done) {
   });
 
   let startWindow = getMostRecentBrowserWindow();
-  let ele = startWindow.document.getElementById(testName);
+  let ele = startWindow.document.getElementById(makeID(testName));
   assert.ok(ele, 'sidebar element was added');
 
   open().then(function(window) {
-      let ele = window.document.getElementById(testName);
+      let ele = window.document.getElementById(makeID(testName));
       assert.ok(ele, 'sidebar element was added');
 
       sidebar.destroy();
-      assert.ok(!window.document.getElementById(testName), 'sidebar id DNE');
-      assert.ok(!startWindow.document.getElementById(testName), 'sidebar id DNE');
+      assert.ok(!window.document.getElementById(makeID(testName)), 'sidebar id DNE');
+      assert.ok(!startWindow.document.getElementById(makeID(testName)), 'sidebar id DNE');
 
       close(window).then(done, assert.fail);
   })
@@ -84,17 +85,17 @@ exports.testSideBarIsNotInNewPrivateWindows = function(assert, done) {
   });
 
   let startWindow = getMostRecentBrowserWindow();
-  let ele = startWindow.document.getElementById(testName);
+  let ele = startWindow.document.getElementById(makeID(testName));
   assert.ok(ele, 'sidebar element was added');
 
   open(null, { features: { private: true } }).then(function(window) {
-      let ele = window.document.getElementById(testName);
+      let ele = window.document.getElementById(makeID(testName));
       assert.ok(isPrivate(window), 'the new window is private');
       assert.equal(ele, null, 'sidebar element was not added');
 
       sidebar.destroy();
-      assert.ok(!window.document.getElementById(testName), 'sidebar id DNE');
-      assert.ok(!startWindow.document.getElementById(testName), 'sidebar id DNE');
+      assert.ok(!window.document.getElementById(makeID(testName)), 'sidebar id DNE');
+      assert.ok(!startWindow.document.getElementById(makeID(testName)), 'sidebar id DNE');
 
       close(window).then(done, assert.fail);
   })
@@ -109,31 +110,62 @@ exports.testSideBarIsShowingInNewWindows = function(assert, done) {
   });
 
   let startWindow = getMostRecentBrowserWindow();
-  let ele = startWindow.document.getElementById(testName);
+  let ele = startWindow.document.getElementById(makeID(testName));
   assert.ok(ele, 'sidebar element was added');
 
   let oldEle = ele;
   sidebar.once('show', function() {
+    assert.pass('show event fired');
+
     sidebar.once('attach', function() {
-      promise.then(function(window) {
-        let ele = window.document.getElementById(testName);
-        assert.ok(ele, 'sidebar element was added');
-        assert.equal(ele.hasAttribute('checked'), true, 'the sidebar is checked');
-        assert.notEqual(ele, oldEle, 'there are two different sidebars');
+      assert.pass('attach event fired');
+
+      sidebar.once('show', function() {
+        let window = getMostRecentBrowserWindow();
+        assert.notEqual(startWindow, window, 'window is new');
+
+        let sb = window.document.getElementById('sidebar');
+        if (sb && sb.docShell && sb.contentDocument && sb.contentDocument.getElementById('web-panels-browser')) {
+          end();
+        }
+        else {
+          sb.addEventListener('DOMWindowCreated', end, false);console.log(2)
+        }
+
+        function end() {
+          sb.removeEventListener('DOMWindowCreated', end, false);
+          let webPanelBrowser = sb.contentDocument.getElementById('web-panels-browser');
+
+          let ele = window.document.getElementById(makeID(testName));
+
+          assert.ok(ele, 'sidebar element was added 2');
+          assert.equal(ele.hasAttribute('checked'), true, 'the sidebar is checked');
+          assert.notEqual(ele, oldEle, 'there are two different sidebars');
 
           assert.equal(isShowing(sidebar), true, 'the sidebar is showing in new window');
-          sidebar.destroy();
 
-          assert.equal(isShowing(sidebar), false, 'the sidebar is not showing');
-          assert.ok(!window.document.getElementById(testName), 'sidebar id DNE');
-          assert.ok(!startWindow.document.getElementById(testName), 'sidebar id DNE');
+          webPanelBrowser.contentWindow.addEventListener('load', function onload() {
+            webPanelBrowser.contentWindow.addEventListener('load', onload, false);
 
-          close(window).then(done, assert.fail);
+            sidebar.destroy();
+
+            assert.equal(isShowing(sidebar), false, 'the sidebar is not showing');
+            assert.ok(!window.document.getElementById(makeID(testName)), 'sidebar id DNE');
+            assert.ok(!startWindow.document.getElementById(makeID(testName)), 'sidebar id DNE');
+
+            setTimeout(function() {
+              close(window).then(done, assert.fail);
+            });
+          }, false);
+        }
       });
+
+      startWindow.OpenBrowserWindow();
     });
-    let promise = windowPromise(startWindow.OpenBrowserWindow(), 'load').then(focus);
   });
+
   show(sidebar);
+  assert.pass('showing the sidebar');
 }
 
 exports.testAddonGlobal = function(assert, done) {
@@ -144,13 +176,22 @@ exports.testAddonGlobal = function(assert, done) {
     url: data.url('test-sidebar-addon-global.html')
   });
 
-  sidebar.on('show', function({ worker }) {
-    worker.on('message', function(msg) {
-      assert.equal(msg, 'hello sidebar add-on');
-      sidebar.destroy();
-      done();
+  sidebar.on('attach', function(worker) {
+    assert.pass('sidebar was attached');
+    assert.ok(!!worker, 'attach event has worker');
+
+    worker.port.once('X', function(msg) {
+      assert.equal(msg, '1', 'got event from worker');
+
+      worker.port.on('X', function(msg) {
+        assert.equal(msg, '123', 'the final message is correct');
+
+        sidebar.destroy();
+
+        done();
+      });
+      worker.port.emit('X', msg + '2');
     })
-    worker.postMessage();
   });
   show(sidebar);
 }
