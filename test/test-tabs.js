78b25013a293de60b59c1d849fc7b3d5667b6e5c
Erik Vold <evold@mozilla.com>
2013-08-14 13:21:45 -0700

    Bug 905354 - Update test-tabs.js file
    Using the module.metadata instead of a hack to check if the app is supported

--- test/test-tabs.js
+++ test/test-tabs.js
@@ -3,21 +3,20 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 'use strict';
 
+module.metadata = {
+  'engines': {
+    'Firefox': '*',
+    'Fennec': '*'
+  }
+};
+
 const app = require('sdk/system/xul-app');
 
-if (app.is('Firefox')) {
-  module.exports = require('./tabs/test-firefox-tabs');
-}
-else if (app.is('Fennec')) {
+if (app.is('Fennec')) {
   module.exports = require('./tabs/test-fennec-tabs');
 }
 else {
-  require('sdk/test').run({
-    'test Unsupported Application': function Unsupported (assert) {
-      assert.pass(
-        'The tabs module currently supports only Firefox and Fennec.' +
-        'In the future we would like it to support other applications, however.'
-      );
-    }
-  });
+  module.exports = require('./tabs/test-firefox-tabs');
 }
+
+require('sdk/test').run(exports);
