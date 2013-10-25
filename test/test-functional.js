9f1b9334dcea337ffb77c2e972f78d6e1d3a3162
Irakli Gozalishvili <rfobic@gmail.com>
2013-10-25 15:34:57 -0700

    Merge oops and functional.

--- test/test-functional.js
+++ test/test-functional.js
@@ -5,7 +5,8 @@
 
 const { setTimeout } = require('sdk/timers');
 const utils = require('sdk/lang/functional');
-const { invoke, defer, partial, compose, memoize, once, delay, wrap, curry, chain } = utils;
+const { invoke, defer, partial, compose, memoize, once,
+        delay, wrap, curry, chainable, field, query, isInstance } = utils;
 const { LoaderWithHookedConsole } = require('sdk/test/loader');
 
 exports['test forwardApply'] = function(assert) {
@@ -195,21 +196,6 @@ exports['test once with argument'] = function(assert) {
   assert.equal(n, 1, 'only incremented once');
 };
 
-exports['test chain'] = function (assert) {
-  let Player = function () { this.volume = 5; };
-  Player.prototype = {
-    setBand: chain(function (band) this.band = band),
-    incVolume: chain(function () this.volume++)
-  };
-  let player = new Player();
-  player
-    .setBand('Animals As Leaders')
-    .incVolume().incVolume().incVolume().incVolume().incVolume().incVolume();
-
-  assert.equal(player.band, 'Animals As Leaders', 'passes arguments into chained');
-  assert.equal(player.volume, 11, 'accepts no arguments in chain');
-};
-
 exports['test complement'] = assert => {
   let { complement } = require("sdk/lang/functional");
 
@@ -316,6 +302,90 @@ exports["test when"] = assert => {
 
   assert.deepEqual(axis(new Point(1, 4)), [1, 4]);
   assert.deepEqual(axis({ foo: "bar" }), [0, 0]);
+};
+
+exports["test chainable"] = function(assert) {
+  let Player = function () { this.volume = 5; };
+  Player.prototype = {
+    setBand: chainable(function (band) this.band = band),
+    incVolume: chainable(function () this.volume++)
+  };
+  let player = new Player();
+  player
+    .setBand('Animals As Leaders')
+    .incVolume().incVolume().incVolume().incVolume().incVolume().incVolume();
+
+  assert.equal(player.band, 'Animals As Leaders', 'passes arguments into chained');
+  assert.equal(player.volume, 11, 'accepts no arguments in chain');
+};
+
+exports["test field"] = (assert) => {
+  let Num = field("constructor", 0);
+  assert.equal(Num.name, Number.name);
+  assert.ok(typeof(Num), "function");
+
+  let x = field("x");
+
+  [
+    [field("foo", { foo: 1 }), 1],
+    [field("foo")({ foo: 1 }), 1],
+    [field("bar", {}), undefined],
+    [field("bar")({}), undefined],
+    [field("hey", undefined), undefined],
+    [field("hey")(undefined), undefined],
+    [field("how", null), null],
+    [field("how")(null), null],
+    [x(1), undefined],
+    [x(undefined), undefined],
+    [x(null), null],
+    [x({ x: 1 }), 1],
+    [x({ x: 2 }), 2],
+  ].forEach(([actual, expected]) => assert.equal(actual, expected));
+};
+
+exports["test query"] = (assert) => {
+  let Num = query("constructor", 0);
+  assert.equal(Num.name, Number.name);
+  assert.ok(typeof(Num), "function");
+
+  let x = query("x");
+  let xy = query("x.y");
+
+  [
+    [query("foo", { foo: 1 }), 1],
+    [query("foo")({ foo: 1 }), 1],
+    [query("foo.bar", { foo: { bar: 2 } }), 2],
+    [query("foo.bar")({ foo: { bar: 2 } }), 2],
+    [query("foo.bar", { foo: 1 }), undefined],
+    [query("foo.bar")({ foo: 1 }), undefined],
+    [x(1), undefined],
+    [x(undefined), undefined],
+    [x(null), null],
+    [x({ x: 1 }), 1],
+    [x({ x: 2 }), 2],
+    [xy(1), undefined],
+    [xy(undefined), undefined],
+    [xy(null), null],
+    [xy({ x: 1 }), undefined],
+    [xy({ x: 2 }), undefined],
+    [xy({ x: { y: 1 } }), 1],
+    [xy({ x: { y: 2 } }), 2]
+  ].forEach(([actual, expected]) => assert.equal(actual, expected));
+};
+
+exports["test isInstance"] = (assert) => {
+  function X() {}
+  function Y() {}
+  let isX = isInstance(X);
+
+  [
+    isInstance(X, new X),
+    isInstance(X)(new X),
+    !isInstance(X, new Y),
+    !isInstance(X)(new Y),
+    isX(new X),
+    !isX(new Y)
+  ].forEach(x => assert.ok(x));
 };
 
 require('test').run(exports);
