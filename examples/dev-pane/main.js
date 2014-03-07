5b50d17cdc506771530a0aeb0634af580234b63d
Luca Greco <luca.greco@alcacoop.it>
2014-03-07 14:32:12 +0100

    prototype remote inspectTarget RDP actor
    - temporary hack to register custom protocol.js actor created from an addon-sdk addon
    - prototyped InspectTarget class
    - use InspectTarget in the "dev/pane" addon-sdk module
    - update "examples/dev-pane" example to test the prototype

--- examples/dev-pane/main.js
+++ examples/dev-pane/main.js
@@ -9,6 +9,34 @@ const pane = new Pane({
   tooltip: "My example pane",
   icon: "./favicon.ico",
   url: "./index.html",
+  onAttach: function(target) {
+    console.log("DEVTOOL TARGET", target.isApp, target.isLocal, target.isRemote);
+
+    // subcribe target tab worker attached event
+    target.on("target-tab-attached", function () {
+      console.log("TARGET TAB ATTACHED", arguments);
+      target.postMessageToContentScript({prova: 123}, "*");
+    });
+
+    // subcribe received messages from target tab worker
+    target.on("content-script-message", function () {
+      console.log("CONTENT SCRIPT MESSAGE", arguments);
+    });
+
+    // NOTE: configure instrumentation on attach
+    target.setupInstrumentation({
+      // content script code
+      // OR contentScriptURL: "content-script.js",
+      contentScriptCode: "self.postMessage({ name: 'prova' });" +
+        "self.on('message', (data) => console.log('message receive', data));" +
+        "console.log('CONTENT SCRIPT LOADED');",
+      // javascript instrumentation code to inject in the target tab
+      // OR injectJavascriptURL: "content-script.js",
+      injectJavascriptCode: "console.log('CODE INJECTED ON PAGE');",
+      // optionally reload the target tab on setup
+      reload: false
+    });
+  },
   onMessage: function(event) {
     console.log("Received message from pane", event, event.source);
 
