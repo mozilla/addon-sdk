6b5a3d1eeb0f105a2a98d8520b3c1f7983463424
Irakli Gozalishvili <rfobic@gmail.com>
2014-03-26 18:47:49 -0700

    Refine API further.

--- examples/actor-repl/index.js
+++ examples/actor-repl/index.js
@@ -1,15 +1,38 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
 
-const { Pane } = require("dev/pane");
+const { Panel } = require("dev/pane");
+const { Tool } = require("dev/toolbox");
+const { Class } = require("sdk/core/heritage");
 
-const pane = new Pane({
+
+const REPLPanel = Class({
+  extends: Panel,
   label: "Actor REPL",
   tooltip: "Firefox debugging protocol REPL",
   icon: "./robot.png",
   url: "./index.html",
-  onAttach: event => {
-    event.source.postMessage("RDP", event.origin, event.ports);
+  setup: function({debuggee}) {
+    this.debuggee = debuggee;
+  },
+  dispose: function() {
+    delete this.debuggee;
+  },
+  onReady: function() {
+    console.log("READY");
+    this.debuggee.start();
+    this.postMessage("RDP", [this.debuggee]);
+  },
+  onLoad: function() {
+    console.log("LOAD");
   }
+});
+exports.REPLPanel = REPLPanel;
+
+
+const replTool = new Tool({
+  name: "repl",
+  panels: { repl: REPLPanel }
 });
