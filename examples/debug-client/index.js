1d35d787d838194a9dd4b308c6048501194d3279
Irakli Gozalishvili <rfobic@gmail.com>
2014-05-05 16:53:56 -0700

    Cleanup implementations.

--- examples/debug-client/index.js
+++ examples/debug-client/index.js
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-const { Panel } = require("dev/pane");
+const { Panel } = require("dev/panel");
 const { Tool } = require("dev/toolbox");
 const { Class } = require("sdk/core/heritage");
 
