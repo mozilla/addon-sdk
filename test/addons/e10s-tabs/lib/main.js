d7c9af24348d3e1101a6a7d407596d21e494836d
Dave Townsend <dtownsend@oxymoronical.com>
2014-08-07 10:43:52 -0700

    Open a new e10s window before each test. It should be easy to remove this block when Nightly goes all-e10s.

--- test/addons/e10s-tabs/lib/main.js
+++ test/addons/e10s-tabs/lib/main.js
@@ -5,15 +5,41 @@
 
 const { merge } = require('sdk/util/object');
 const { version } = require('sdk/system');
-
-merge(module.exports, require('./test-tab'));
-merge(module.exports, require('./test-tab-events'));
-merge(module.exports, require('./test-tab-observer'));
-merge(module.exports, require('./test-tab-utils'));
+const { getMostRecentBrowserWindow } = require('sdk/window/utils');
+const { promise: windowPromise, close, focus } = require('sdk/window/helpers');
 
 // run e10s tests only on builds from trunk, fx-team, Nightly..
 if (!version.endsWith('a1')) {
   module.exports = {};
 }
+
+function openE10sWindow() {
+  let window = getMostRecentBrowserWindow().OpenBrowserWindow({ remote: true });
+  return windowPromise(window, 'load').then(focus);
+}
+
+function makeE10sTests(exports) {
+  let newExports = {};
+
+  for (let key of Object.keys(exports)) {
+    if (typeof(exports[key]) == "function" && key.substring(0, 4) == "test") {
+      let testFunction = exports[key];
+      newExports[key] = function(assert, done) {
+        openE10sWindow().then(window => {
+          testFunction(assert, () => {
+            close(window).then(done);
+          });
+        });
+      }
+    }
+  }
+
+  return newExports;
+}
+
+merge(module.exports, makeE10sTests(require('./test-tab')));
+merge(module.exports, makeE10sTests(require('./test-tab-events')));
+merge(module.exports, makeE10sTests(require('./test-tab-observer')));
+merge(module.exports, makeE10sTests(require('./test-tab-utils')));
 
 require('sdk/test/runner').runTestsFromModule(module);
