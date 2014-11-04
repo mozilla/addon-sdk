73d2e4d4611df19a0b3f68e066e146a32950c721
Erik Vold <evold@mozilla.com>
2014-11-03 16:37:28 -0800

    Bug 1093385 Part 1 - Adding .travis.yml and supporting scripts

---
+++ bin/node-scripts/test.addons.js
@@ -0,0 +1,75 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+var utils = require("./utils");
+var path = require("path");
+var fs = require("fs");
+var chai = require("chai");
+var expect = chai.expect;
+var assert = chai.assert;
+var exec = utils.exec;
+
+var examplesPath = path.join(__dirname, "..", "..", "examples");
+var addonsPath = path.join(__dirname, "..", "..", "test", "addons");
+
+var binary = process.env.JPM_FIREFOX_BINARY || "nightly";
+
+describe("jpm test sdk examples", function () {
+  //beforeEach(utils.setup);
+  //afterEach(utils.tearDown);
+
+  fs.readdirSync(examplesPath)
+  .filter(fileFilter.bind(null, examplesPath))
+  .forEach(function (file) {
+    it(file, function (done) {
+      var addonPath = path.join(examplesPath, file);
+      process.chdir(addonPath);
+
+      var options = { cwd: addonPath, env: { JPM_FIREFOX_BINARY: binary }};
+      if (process.env.DISPLAY) {
+        options.env.DISPLAY = process.env.DISPLAY;
+      }
+      var proc = exec("test -v", options, function (err, stdout, stderr) {
+        expect(err).to.not.be.ok;
+        expect(stderr).to.not.be.ok;
+        expect(stdout).to.contain("All tests passed!");
+        done();
+      });
+    });
+  });
+});
+
+describe("jpm test sdk addons", function () {
+  //beforeEach(utils.setup);
+  //afterEach(utils.tearDown);
+
+  fs.readdirSync(addonsPath)
+  .filter(fileFilter.bind(null, addonsPath))
+  .forEach(function (file) {
+    it(file, function (done) {
+      var addonPath = path.join(addonsPath, file);
+      process.chdir(addonPath);
+
+      var options = { cwd: addonPath, env: { JPM_FIREFOX_BINARY: binary }};
+      if (process.env.DISPLAY) {
+        options.env.DISPLAY = process.env.DISPLAY;
+      }
+      var proc = exec("run -v", options, function (err, stdout, stderr) {
+        expect(err).to.not.be.ok;
+        expect(stderr).to.not.be.ok;
+        expect(stdout).to.contain("All tests passed!");
+        done();
+      });
+    });
+  });
+});
+
+function fileFilter(root, file) {
+  if (/^(invalid|e10s)-/.test(file)) {
+    return false;
+  }
+  var stat = fs.statSync(path.join(root, file))
+  return (stat && stat.isDirectory());
+}
---
+++ bin/node-scripts/utils.js
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+var _ = require("lodash");
+var path = require("path");
+var child_process = require("child_process");
+
+function exec (args, options, callback) {
+  options = options || {};
+  var env = _.extend({}, options.env, process.env);
+
+  return child_process.exec("node " + path.join(__dirname, "../../node_modules/jpm/bin/jpm") + " " + args, {
+    cwd: options.cwd || tmpOutputDir,
+    env: env
+  }, function (err, stdout, stderr) {
+    if (callback)
+      callback.apply(null, arguments);
+    else if (err)
+      throw err;
+  });
+}
+exports.exec = exec;
