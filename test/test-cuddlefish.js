9f8bca2d3e67369237d05784420479b1a93de96d
Erik Vold <evold@mozilla.com>
2014-12-12 15:44:23 -0800

    Bug 1108785 bug fix - ReferenceError: can't access lexical declaration  before initialization a=me

--- test/test-cuddlefish.js
+++ test/test-cuddlefish.js
@@ -1,13 +1,13 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 'use strict';
 
 const packaging = require('@loader/options');
+const get = require;
 
 exports['test loader'] = function(assert) {
-  const { Loader, Require, unload, override } = require('sdk/loader/cuddlefish');
+  let { Loader, Require, unload, override } = get('sdk/loader/cuddlefish');
   var prints = [];
   function print(message) {
     prints.push(message);
