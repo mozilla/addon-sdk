fca2f57733f801e94f63a3b3962ad8c4fced07ed
Jan Odvarko <odvarko@gmail.com>
2014-12-16 13:49:04 +0100

    Bug 1057346 - Support for relative style URL

--- examples/theme/lib/main.js
+++ examples/theme/lib/main.js
@@ -3,23 +3,21 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-const self = require("sdk/self");
-
 const { Tool } = require("dev/toolbox");
 const { Class } = require("sdk/core/heritage");
-const { Style } = require("sdk/stylesheet/style");
 const { onEnable, onDisable } = require("dev/theme/hooks");
 const { Theme, LightTheme } = require("dev/theme");
 
 /**
- * This object represents a new theme registered within the Toolbox
- * You can activate it by clicking on "My Light Theme" in the Options
- * panel. The theme derives styles from built-in Light theme.
+ * This object represents a new theme registered within the Toolbox.
+ * You can activate it by clicking on "My Light Theme" theme option
+ * in the Options panel.
+ * Note that the new theme derives styles from built-in Light theme.
  */
 const MyTheme = Theme({
   name: "mytheme",
   label: "My Light Theme",
-  styles: [LightTheme, self.data.url("theme.css")],
+  styles: [LightTheme, "./theme.css"],
 
   onEnable: function(window, oldTheme) {
     console.log("myTheme.onEnable; method override " +
