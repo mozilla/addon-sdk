7caca5b489c113ba64cb7544707d9b63dea1856d
Jan Odvarko <odvarko@gmail.com>
2014-12-16 13:49:45 +0100

    Bug 1057346 - Remove helper script

--- examples/theme/run.sh
+++
@@ -1,5 +0,0 @@
-
-jpm run \
-    --overload C:/src/github.com/mozilla/addon-sdk/ \
-    --binary C:/mozilla-dev/firefox/firefox-nightly/firefox.exe \
-    --profile /C/Users/Honza/AppData/Roaming/Mozilla/Firefox/Profiles/ug2k4vci.nightlyNext002
