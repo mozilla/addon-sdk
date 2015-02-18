a5403f6e1e1a1baa7c9a02ea547d890c1359e6bc
Jan Odvarko <odvarko@gmail.com>
2015-02-18 17:53:27 +0100

    Simplify test for theme example

--- examples/theme/test/test-main.js
+++ examples/theme/test/test-main.js
@@ -1,12 +1,10 @@
-var main = require("./main");
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ "use strict";
 
-exports["test main"] = function(assert) {
-  assert.pass("Unit test running!");
-};
-
-exports["test main async"] = function(assert, done) {
-  assert.pass("async Unit test running!");
-  done();
+exports.testMain = function(assert) {
+  assert.pass("TODO: Write some tests.");
 };
 
 require("sdk/test").run(exports);
