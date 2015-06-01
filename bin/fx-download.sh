7b15ea5b33546285238a2d8d3f16b98e9c599195
Erik Vold <evold@mozilla.com>
2015-05-31 21:52:26 -0700

    Try using mozilla-download, can not figure out how to get a debug build at the moment

--- bin/fx-download.sh
+++ bin/fx-download.sh
@@ -3,5 +3,5 @@
 if [ "$JPM_FX_DEBUG" = "1" ]; then
   fx-download --branch nightly -c prerelease --host ftp.mozilla.org ../firefox --debug
 else
-  fx-download --branch nightly -c prerelease --host ftp.mozilla.org ../firefox
+  mozilla-download ../firefox --branch mozilla-central --product firefox
 fi
